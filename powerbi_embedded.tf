6de3e99c6f10976b50d8f645647e27fd1cdb9681
Kévin DELMONT <kevin.delmont@visiativ.com>
2023-06-29 17:21:12 +0200

    fix

--- powerbi_embedded.tf
+++ powerbi_embedded.tf
@@ -1,7 +1,7 @@
 
-module "azurerm_powerbi_embedded" {
+module "powerbi_embedded" {
   source   = "./modules/powerbi_embedded"
-  for_each = local.powerbi_embedded.azurerm_powerbi_embedded
+  for_each = local.powerbi_embedded
 
   client_config       = local.client_config
   global_settings     = local.global_settings
@@ -12,20 +12,8 @@ module "azurerm_powerbi_embedded" {
   administrators      = each.value.administrators
   sku_name            = each.value.sku_name
   mode                = try(each.value.mode, "Gen1")
-  tags                = try(each.value.tags, null)
+  base_tags           = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
 }
-
-# module "azurerm_powerbi_embedded" {
-#   source = "./modules/powerbi_embedded"
-
-#   name           = var.name
-#   location       = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
-#   sku_name       = var.sku_name
-#   administrators = var.administrators
-#   mode           = try(var.mode, "Gen1")
-#   tags           = try(var.tags, null)
-# }
-
 
 output "powerbi_embedded" {
   value = module.azurerm_powerbi_embedded
