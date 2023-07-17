367dd0ed0f13ce272846c0ee9cbbf971e4847e66
shanoor <17402800+shanoor@users.noreply.github.com>
2023-07-17 15:12:44 +0200

    maj

--- modules/security/disk_encryption_set_external/disk_encryption_set.tf
+++ modules/security/disk_encryption_set_external/disk_encryption_set.tf
@@ -14,7 +14,7 @@ resource "azurerm_disk_encryption_set" "encryption_set" {
     type = try(var.settings.identity.type, "SystemAssigned")
 
     # if type contains UserAssigned, `identity_ids` is mandatory
-    identity_ids = try(var.settings.identity.type, "") == "UserAssigned" ? flatten([
+    identity_ids = try(regex("UserAssigned", var.settings.identity.type), null) != null ? flatten([
       for managed_identity in var.settings.identity.managed_identities : [
         var.managed_identities[try(managed_identity.lz_key, var.client_config.landingzone_key)][managed_identity.key].id
       ]
