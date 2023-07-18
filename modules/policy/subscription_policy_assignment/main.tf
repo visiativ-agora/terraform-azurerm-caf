b70a745552ed1d4c3241e30e1c396f555d7dc45d
Kévin DELMONT <kevin.delmont@visiativ.com>
2023-07-18 14:57:45 +0200

    remove doublon and fix main.tf

--- modules/policy/subscription_policy_assignment/main.tf
+++ modules/policy/subscription_policy_assignment/main.tf
@@ -1,4 +1,4 @@
-erraform {
+terraform {
   required_providers {
     azurecaf = {
       source = "aztfmod/azurecaf"
--- modules/policy/subscription_policy_assignment/variables.tf
+++ modules/policy/subscription_policy_assignment/variables.tf
@@ -20,11 +20,6 @@ variable "name" {
   type        = string
 }
 
-variable "location" {
-  description = "(Required) Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
-  type        = string
-}
-
 variable "resource_group_name" {
   description = "Resource group object"
 }
