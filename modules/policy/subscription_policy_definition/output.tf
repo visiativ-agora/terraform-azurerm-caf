79a1944695d86b1d285be35ef713b1638242fbde
Kévin DELMONT <kevin.delmont@visiativ.com>
2023-07-18 17:08:44 +0200

    fix output

--- modules/policy/subscription_policy_definition/output.tf
+++ modules/policy/subscription_policy_definition/output.tf
@@ -5,6 +5,6 @@ output "id" {
 
 output "role_definition_ids" {
   description = "A list of role definition id extracted from policy_rule required for remediation"
-  value       = azurerm_policy_definition.azurerm_policy_definition.role_definition_ids
+  value       = azurerm_subscription_policy_definition.subscription_policy_definition.role_definition_ids
 }
 
