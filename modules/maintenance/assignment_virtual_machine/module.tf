41f4ef52fe74dca3ad9cbee9186562a762b4758f
Kévin DELMONT <kevin.delmont@visiativ.com>
2023-08-01 10:50:40 +0200

    test

--- modules/maintenance/assignment_virtual_machine/module.tf
+++ modules/maintenance/assignment_virtual_machine/module.tf
@@ -1,7 +1,8 @@
-resource "azurerm_maintenance_assignment_virtual_machine" "maintenance_assignment_virtual_machine" {  
+resource "azurerm_maintenance_assignment_virtual_machine" "maintenance_assignment_virtual_machine" {
   location                     = var.location
   maintenance_configuration_id = var.maintenance_configuration_id
-  virtual_machine_id           = var.virtual_machine_id
+  #virtual_machine_id           = var.virtual_machine_id
+  virtual_machine_id = var.virtual_machine_id != null ? var.virtual_machine_id : can(each.value.virtual_machine_id) ? each.value.virtual_machine_id : try(local.combined_objects_virtual_machines[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.virtual_machine_key].id, null)
 }
 
 
