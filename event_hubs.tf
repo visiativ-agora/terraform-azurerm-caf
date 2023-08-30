e1fd4cdde8558e72f0383d0ca23ea06e828bbb86
shanoor <17402800+shanoor@users.noreply.github.com>
2023-08-30 19:57:06 +0200

    update

--- event_hubs.tf
+++ event_hubs.tf
@@ -36,8 +36,12 @@ module "event_hub_namespace_auth_rules" {
 }
 
 output "event_hub_namespace_auth_rules" {
-  # value = merge(module.event_hub_namespace_auth_rules, module.event_hub_namespaces.auth_rules, module.diagnostic_event_hub_namespaces.auth_rules)
-  value = module.event_hub_namespace_auth_rules
+  value = merge(
+    module.event_hub_namespace_auth_rules,
+    { for key, eh in module.event_hub_namespaces : key => eh.auth_rules },
+    { for key, eh in module.diagnostic_event_hub_namespaces : key => eh.auth_rules }
+  )
+  # value = module.event_hub_namespace_auth_rules
 }
 
 module "event_hub_namespaces_diagnostics" {
@@ -133,8 +137,11 @@ module "event_hub_auth_rules" {
 }
 
 output "event_hub_auth_rules" {
-  # value = merge(module.event_hub_auth_rules, module.event_hubs.auth_rules)
-  value = module.event_hub_auth_rules
+  value = merge(
+    module.event_hub_auth_rules,
+    { for key, eh in module.event_hubs : key => eh.auth_rules },
+  )
+  # value = module.event_hub_auth_rules
 }
 
 module "event_hub_consumer_groups" {
