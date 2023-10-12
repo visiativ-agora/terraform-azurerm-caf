7191a213cb382193c8fb3a682ed8f3329802a732
Kévin DELMONT <kevin.delmont@visiativ.com>
2023-10-12 11:22:44 +0200

    add example and add standalone scenarios

--- modules/recovery_vault/diagnostics.tf
+++ modules/recovery_vault/diagnostics.tf
@@ -6,5 +6,5 @@ module "diagnostics" {
   resource_id       = azurerm_recovery_services_vault.asr.id
   resource_location = local.location
   diagnostics       = var.diagnostics
-  profiles          = var.diagnostic_profiles
+  profiles          = try(var.settings.diagnostic_profiles, {})
 }
