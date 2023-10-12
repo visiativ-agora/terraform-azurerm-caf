001e4d1e0ab7851755ed5609d0e8643ea53bed85
kevindelmont <133667252+kevindelmont@users.noreply.github.com>
2023-10-12 14:39:59 +0200

    Visiativ/feat/recoveryvault (#10)
    * add diagnostic_profiles for recovery vault
    * add example and add standalone scenarios

--- modules/recovery_vault/diagnostics.tf
+++ modules/recovery_vault/diagnostics.tf
@@ -1,7 +1,8 @@
 
 module "diagnostics" {
   source = "../diagnostics"
-
+  count  = var.diagnostic_profiles == null ? 0 : 1
+  
   resource_id       = azurerm_recovery_services_vault.asr.id
   resource_location = local.location
   diagnostics       = var.diagnostics
