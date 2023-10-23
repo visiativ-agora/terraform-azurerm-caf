1f76a6e396d82403d223cf0095085446cd3168bf
shanoor <17402800+shanoor@users.noreply.github.com>
2023-10-23 15:46:38 +0200

    fix

--- modules/databases/app_config/settings/app_config_setting.tf
+++ modules/databases/app_config/settings/app_config_setting.tf
@@ -36,5 +36,5 @@ resource "azurerm_app_configuration_key" "config" {
   # if value is a keyvault reference, set the correct type, set value to null and set vault_key_reference
   type                = try(each.value.vault_key, null) == null ? "kv" : "vault"
   value               = try(each.value.vault_key, null) == null ? each.value.value : null
-  vault_key_reference = try(each.value.vault_key, null) == null ? null : "${var.keyvaults[try(each.value.vault_key.keyvault.lz_key, var.client_config.landingzone_key)][each.value.vault_key.keyvault.key].vault_uri}/secrets/${each.value.vault_key.secret_name}"
+  vault_key_reference = try(each.value.vault_key, null) == null ? null : "${var.keyvaults[try(each.value.vault_key.keyvault.lz_key, var.client_config.landingzone_key)][each.value.vault_key.keyvault.key].vault_uri}secrets/${each.value.vault_key.secret_name}"
 }
