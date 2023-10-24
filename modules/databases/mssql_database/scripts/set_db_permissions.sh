90656da74e4bc3422fef3eacca791b3a80086af0
shanoor <17402800+shanoor@users.noreply.github.com>
2023-10-24 10:54:29 +0200

    fix sqlcmd script

--- modules/databases/mssql_database/scripts/set_db_permissions.sh
+++ modules/databases/mssql_database/scripts/set_db_permissions.sh
@@ -12,9 +12,11 @@ Connection Timeout = 30" | sudo tee /etc/odbc.ini >/dev/null
 # if SQLADMINPASSWORD is empty, the script will try to use MSI
 if [[ -z "${SQLADMINPASSWORD}" ]]; then
   echo "Authentication = ActiveDirectoryMsi" | sudo tee -a /etc/odbc.ini >/dev/null
-else
-  echo "Uid = sqladmin
-Pwd = ${SQLADMINPASSWORD}" | sudo tee -a /etc/odbc.ini >/dev/null
 fi
 
-sqlcmd -v DBUSERNAMES="${DBUSERNAMES}" DBROLES="${DBROLES}" -S SQLSERVER -d "${SQLCMDDBNAME}" -i "${SQLFILEPATH}" -D
+if [[ -z "${SQLADMINPASSWORD}" ]]; then
+  sqlcmd -v DBUSERNAMES="${DBUSERNAMES}" DBROLES="${DBROLES}" -S SQLSERVER -d "${SQLCMDDBNAME}" -i "${SQLFILEPATH}" -D
+else
+  # if using user/password, we must pass it to the command (user/password in odbc.ini on Linux is not supported)
+  sqlcmd -v DBUSERNAMES="${DBUSERNAMES}" DBROLES="${DBROLES}" -S SQLSERVER -d "${SQLCMDDBNAME}" -i "${SQLFILEPATH}" -D -U sqladmin -P "${SQLADMINPASSWORD}"
+fi
