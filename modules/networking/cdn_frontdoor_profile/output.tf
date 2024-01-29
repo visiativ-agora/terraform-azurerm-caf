ba4f193561f0aad2d983f4edd8139250e343beb7
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-01-29 17:16:29 +0100

    fix

--- modules/networking/cdn_frontdoor_profile/output.tf
+++ modules/networking/cdn_frontdoor_profile/output.tf
@@ -1,8 +1,8 @@
 output "id" {
-  value       = azurerm_cdn_frontdoor_profile.cdn.id
+  value       = azurerm_cdn_frontdoor_profile.cdn_frontdoor_profile.id
   description = "The ID of this Front Door Profile."
 }
 output "resource_guid" {
-  value       = azurerm_cdn_frontdoor_profile.cdn.resource_guid
+  value       = azurerm_cdn_frontdoor_profile.cdn_frontdoor_profile.resource_guid
   description = "The UUID of this Front Door Profile which will be sent in the HTTP Header as the X-Azure-FDID attribute."
 }
