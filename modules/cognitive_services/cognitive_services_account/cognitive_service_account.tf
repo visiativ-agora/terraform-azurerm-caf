15d03c001d47726d7eb947e00545b444a51594c1
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-05-14 10:44:34 +0200

    test

--- modules/cognitive_services/cognitive_services_account/cognitive_service_account.tf
+++ modules/cognitive_services/cognitive_services_account/cognitive_service_account.tf
@@ -60,7 +60,7 @@ resource "azurerm_cognitive_account" "service" {
 
 resource "azurecaf_name" "deployment" {
   name          = var.settings.name
-  resource_type = "azurerm_cognitive_deployment"
+  resource_type = "azurerm_cognitive_account"
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
   clean_input   = true
