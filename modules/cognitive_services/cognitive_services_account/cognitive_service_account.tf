7630b755e955840c4173653a68079e00bdb43274
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-05-14 10:20:31 +0200

    fix azurecafname

--- modules/cognitive_services/cognitive_services_account/cognitive_service_account.tf
+++ modules/cognitive_services/cognitive_services_account/cognitive_service_account.tf
@@ -59,7 +59,7 @@ resource "azurerm_cognitive_account" "service" {
 # }
 
 data "azurecaf_name" "deployment" {
-  name          = var.name
+  name          = var.settings.name
   resource_type = "azurerm_cognitive_deployment"
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
