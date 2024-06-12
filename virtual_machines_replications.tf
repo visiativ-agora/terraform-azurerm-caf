95ed14273109d49de3a2730a2d3b62025a7584e4
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-06-12 17:04:45 +0200

    add try

--- virtual_machines_replications.tf
+++ virtual_machines_replications.tf
@@ -16,11 +16,11 @@ module "vm_replication" {
   recovery_vaults            = local.combined_objects_recovery_vaults
   resource_groups            = local.combined_objects_resource_groups
   storage_accounts           = local.combined_objects_storage_accounts
-  disk_encryption_sets       = local.combined_objects_disk_encryption_sets
+  disk_encryption_sets       = try(local.combined_objects_disk_encryption_sets, null)
   settings                   = each.value
   vnets                      = local.combined_objects_networking
   client_config              = local.client_config
   virtual_machine_os_disk    = module.virtual_machines[each.key].os_disk
-  virtual_machine_data_disks = module.virtual_machines[each.key].data_disks
+  virtual_machine_data_disks = try(module.virtual_machines[each.key].data_disks, null)
   virtual_machine_nics       = module.virtual_machines[each.key].nics
 }
