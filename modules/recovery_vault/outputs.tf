3db8ff3e71c165b62547fc3df9bdb32c63903924
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-06-13 15:36:04 +0200

    fix output

--- modules/recovery_vault/outputs.tf
+++ modules/recovery_vault/outputs.tf
@@ -54,3 +54,8 @@ output "replication_plan_id" {
   description = "Output the set of replication plan id"
   value       = azurerm_site_recovery_replication_recovery_plan.replication_plan.id
 }
+
+output "replication_plan_ids" {
+  description = "Output the set of replication plan ids"
+  value       = { for key, value in azurerm_site_recovery_replication_recovery_plan.replication_plan : key => plan.id }
+}
