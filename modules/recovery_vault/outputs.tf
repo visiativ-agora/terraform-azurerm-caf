0380aaac245f061cf93caf99eb20b9139d08814a
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-06-13 15:48:34 +0200

    fix

--- modules/recovery_vault/outputs.tf
+++ modules/recovery_vault/outputs.tf
@@ -52,5 +52,5 @@ output "protection_containers" {
 
 output "replication_plan_ids" {
   description = "Output the set of replication plan ids"
-  value       = { for key, value in azurerm_site_recovery_replication_recovery_plan.replication_plan : key => plan.id }
+  value       = { for key, value in azurerm_site_recovery_replication_recovery_plan.replication_plan : key => value.id }
 }
