f2736f066f4ef189ed35823647bc3ac9522e3893
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-06-14 09:22:10 +0200

    fix condition

--- modules/recovery_vault/azure_recovery_plan.tf
+++ modules/recovery_vault/azure_recovery_plan.tf
@@ -12,7 +12,7 @@ resource "azurerm_site_recovery_replication_recovery_plan" "replication_plan" {
 
   dynamic "shutdown_recovery_group" {
     # for_each = try(each.value.shutdown_recovery_group, [])
-    for_each = try(each.value.shutdown_recovery_group, null) != null ? [1] : []
+    for_each = try(each.value.shutdown_recovery_group, null) != null ? [each.value.shutdown_recovery_group] : []
     content {
       dynamic "pre_action" {
         for_each = try(shutdown_recovery_group.value.pre_action, [])
@@ -45,8 +45,7 @@ resource "azurerm_site_recovery_replication_recovery_plan" "replication_plan" {
   }
 
   dynamic "failover_recovery_group" {
-    # for_each = try(each.value.failover_recovery_group, [])
-    for_each = try(each.value.failover_recovery_group, null) != null ? [1] : []
+    for_each = try(each.value.failover_recovery_group, null) != null ? [each.value.failover_recovery_group] : []
     content {
       dynamic "pre_action" {
         for_each = try(failover_recovery_group.value.pre_action, [])
@@ -79,8 +78,7 @@ resource "azurerm_site_recovery_replication_recovery_plan" "replication_plan" {
   }
 
   dynamic "boot_recovery_group" {
-    # for_each = try(each.value.boot_recovery_group, [])
-    for_each = try(each.value.boot_recovery_group, null) != null ? [1] : []
+    for_each = try(each.value.boot_recovery_group, null) != null ? [each.value.boot_recovery_group] : []
     content {
       replicated_protected_items = try(each.value.virtual_machine, [])
       
