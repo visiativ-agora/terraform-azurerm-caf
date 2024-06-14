0a306da807d3ce7e65424c2c78e9c7a1a8612537
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-06-14 11:04:04 +0200

    test

--- modules/recovery_vault/azure_recovery_plan.tf
+++ modules/recovery_vault/azure_recovery_plan.tf
@@ -12,11 +12,13 @@ resource "azurerm_site_recovery_replication_recovery_plan" "replication_plan" {
 
   dynamic "shutdown_recovery_group" {
     # for_each = try(each.value.shutdown_recovery_group, [])
-    for_each = try(each.value.shutdown_recovery_group, null) != null ? [each.value.shutdown_recovery_group] : []
+    # for_each = try(each.value.shutdown_recovery_group, null) != null ? [each.value.shutdown_recovery_group] : {}
+    for_each = try(var.settings.replication_plan.shutdown_recovery_group, null) != null ? [var.settings.replication_plan.shutdown_recovery_group] : {}
     content {
       dynamic "pre_action" {        
         # for_each = try(shutdown_recovery_group.value.pre_action, null) != null ? [shutdown_recovery_group.value.pre_action] : []
-        for_each = try(shutdown_recovery_group.value.pre_action, null) != null ? [1] : []
+        # for_each = try(shutdown_recovery_group.value.pre_action, null) != null ? [1] : []
+        for_each = try(var.var.settings.replication_plan.shutdown_recovery_group.pre_action, null) != null ? [var.settings.areplication_plan.shutdown_recovery_group.pre_action] : []
         content {
           name                      = pre_action.value.name
           type                      = pre_action.value.type
@@ -48,7 +50,7 @@ resource "azurerm_site_recovery_replication_recovery_plan" "replication_plan" {
   }
 
   dynamic "failover_recovery_group" {
-    for_each = try(each.value.failover_recovery_group, null) != null ? [each.value.failover_recovery_group] : []
+    for_each = try(each.value.failover_recovery_group, null) != null ? [each.value.failover_recovery_group] : {}
     content {
       dynamic "pre_action" {
         # for_each = try(failover_recovery_group.value.pre_action, [])
@@ -85,7 +87,7 @@ resource "azurerm_site_recovery_replication_recovery_plan" "replication_plan" {
   }
 
   dynamic "boot_recovery_group" {
-    for_each = try(each.value.boot_recovery_group, null) != null ? [each.value.boot_recovery_group] : []
+    for_each = try(each.value.boot_recovery_group, null) != null ? [each.value.boot_recovery_group] : {}
     content {      
       replicated_protected_items = try(each.value.boot_recovery_group.virtual_machines, [])
      
