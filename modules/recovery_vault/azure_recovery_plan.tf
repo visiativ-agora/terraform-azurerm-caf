97c745cdb6f85cbfc80a1acb4ba5866ee490fdf7
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-06-14 10:01:40 +0200

    debug

--- modules/recovery_vault/azure_recovery_plan.tf
+++ modules/recovery_vault/azure_recovery_plan.tf
@@ -84,7 +84,13 @@ resource "azurerm_site_recovery_replication_recovery_plan" "replication_plan" {
   dynamic "boot_recovery_group" {
     for_each = try(each.value.boot_recovery_group, null) != null ? [each.value.boot_recovery_group] : []
     content {      
-      replicated_protected_items = try(each.value.virtual_machines, ["test"])
+      # replicated_protected_items = try(each.value.virtual_machines, ["test"])
+      replicated_protected_items = try(each.value.boot_recovery_group.virtual_machines, [])
+
+      # Temporary output for debugging
+      output "boot_recovery_group_debug" {
+        value = each.value.boot_recovery_group
+      }
                
       dynamic "pre_action" {
         # for_each = try(boot_recovery_group.value.pre_action, [])
