53ae588736212e775e52da4c76fca685a5838a53
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-06-28 14:46:45 +0200

    fix output

--- modules/recovery_vault/recovery_plan/outputs.tf
+++ modules/recovery_vault/recovery_plan/outputs.tf
@@ -1,4 +1,4 @@
 output "id" {
   description = "The IDs of the Recovery Plans."
-  value = { for k, v in azurerm_site_recovery_replication_recovery_plan.replication_plan : k => v.id }
+  value       = azurerm_site_recovery_replication_recovery_plan.replication_plan.id
 }
