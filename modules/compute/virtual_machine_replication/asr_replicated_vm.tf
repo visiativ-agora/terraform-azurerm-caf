1dac457d45890cfb303ed02941bfa9f4916c3bd7
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-07-01 09:24:24 +0200

    add lower source_vm_id = lower(var.virtual_machine_id)

--- modules/compute/virtual_machine_replication/asr_replicated_vm.tf
+++ modules/compute/virtual_machine_replication/asr_replicated_vm.tf
@@ -26,7 +26,7 @@ resource "azurerm_site_recovery_replicated_vm" "replication" {
     try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.replication.vault_key].recovery_fabrics[var.settings.replication.source.recovery_fabric_key].name, null),
     try(var.recovery_vaults[var.settings.replication.lz_key][var.settings.replication.vault_key].recovery_fabrics[var.settings.replication.source.recovery_fabric_key].name, null)
   )
-  source_vm_id = var.virtual_machine_id
+  source_vm_id = lower(var.virtual_machine_id)
   source_recovery_protection_container_name = coalesce(
     try(var.settings.replication.source.protection_container_name, null),
     try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.replication.vault_key].protection_containers[var.settings.replication.source.protection_container_key].name, null),
