ce02270595bbb5e0a2e7d7a97931f22160c2029b
kevindelmont <133667252+kevindelmont@users.noreply.github.com>
2024-07-15 09:41:52 +0200

    Add cognitive deployment module (#26)
    * add cognitive service deployment module

--- modules/cognitive_services/cognitive_services_account/output.tf
+++ modules/cognitive_services/cognitive_services_account/output.tf
@@ -3,6 +3,11 @@ output "id" {
   value       = azurerm_cognitive_account.service.id
 }
 
+output "deployment_id" {
+  description = "The ID of the Deployment for Azure Cognitive Services Account."
+  value       = azurerm_cognitive_account.deployment.id
+}
+
 output "endpoint" {
   description = "The endpoint used to connect to the Cognitive Service Account."
   value       = azurerm_cognitive_account.service.endpoint
