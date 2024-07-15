0efa72b5da917d7038056e965d92399ef9e1b7b7
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-07-15 11:13:52 +0200

    fix ouput azurerm_cognitive_deployment

--- modules/cognitive_services/cognitive_services_account/output.tf
+++ modules/cognitive_services/cognitive_services_account/output.tf
@@ -5,7 +5,7 @@ output "id" {
 
 output "deployment_id" {
   description = "The ID of the Deployment for Azure Cognitive Services Account."
-  value       = azurerm_cognitive_account.deployment.id
+  value       = azurerm_cognitive_deployment.id
 }
 
 output "endpoint" {
