839b32e1d20fb46e1aa285bf652dd2231f35092e
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-07-16 17:10:07 +0200

    fi

--- modules/maintenance/assignment_dynamic_scope/main.tf
+++ modules/maintenance/assignment_dynamic_scope/main.tf
@@ -26,15 +26,15 @@ locals {
   #   []
   # )
 
-  resource_groups = flatten(
+resource_groups = flatten(coalesce(
     try([
       for rg_key in try(var.settings.filter.resource_group_key, []) : 
         try(var.resource_groups[var.client_config.landingzone_key][rg_key], [])
-    ], []),
+      ], null),
     try([
       for rg_key in try(var.settings.filter.resource_group.key, []) : 
-        try(var.resource_groups[var.client_config.landingzone_key][rg_key], [])
-    ], []),
+        try(var.resource_groups[var.settings.filter.resource_group.lz_key][rg_key], [])
+      ], null),
     []
-  )
+  ))
 }
