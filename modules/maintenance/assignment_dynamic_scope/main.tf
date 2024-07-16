4363a7730cc71d5768e73b2bdc4327ede10f9afc
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-07-16 16:55:03 +0200

    test

--- modules/maintenance/assignment_dynamic_scope/main.tf
+++ modules/maintenance/assignment_dynamic_scope/main.tf
@@ -12,33 +12,29 @@ locals {
   }
   tags     = merge(var.base_tags, local.module_tag, try(var.tags, null))
 
-#  resource_group = coalesce(
-#     try(var.resource_groups[var.client_config.landingzone_key][var.settings.filter.resource_group_key], null),
-#     try(var.resource_groups[var.settings.filter.resource_group.lz_key][var.settings.filter.resource_group.resource_group_key], null),
-#     try(var.resource_groups[var.client_config.landingzone_key][var.settings.filter.resource_group.key], null),
-#     try(var.resource_groups[var.settings.filter.resource_group.lz_key][var.settings.filter.resource_group.key], null)
-#   )
   # resource_groups = coalesce(
-  #     [
-  #       for rg_key in var.settings.filter.resource_group_key : [
-  #         try(var.resource_groups[var.client_config.landingzone_key][rg_key], [])
-  #       ]
-  #     ],
-  #     [] 
+  #   try([
+  #     for rg_key in try(var.settings.filter.resource_group_key, []) : [
+  #       try(var.resource_groups[var.client_config.landingzone_key][rg_key], [])
+  #     ]
+  #   ]),
+  #   try([
+  #     for rg_key in try(var.settings.filter.resource_group.key, []) : [
+  #       try(var.resource_groups[var.client_config.landingzone_key][rg_key], [])
+  #     ]
+  #   ]),
+  #   []
   # )
 
-
-  resource_groups = coalesce(
+  resource_groups = flatten(coalesce(
     try([
-      for rg_key in try(var.settings.filter.resource_group_key, []) : [
+      for rg_key in try(var.settings.filter.resource_group_key, []) : 
         try(var.resource_groups[var.client_config.landingzone_key][rg_key], [])
-      ]
-    ]),
+    ], []),
     try([
-      for rg_key in try(var.settings.filter.resource_group.key, []) : [
-        try(var.resource_groups[var.client_config.landingzone_key][rg_key], [])
-      ]
-    ]),
+      for rg_key in try(var.settings.filter.resource_group.key, []) : 
+        try(var.resource_groups[var.settings.filter.resource_group.lz_key][rg_key], [])
+    ], []),
     []
-  )
+  ))
 }
