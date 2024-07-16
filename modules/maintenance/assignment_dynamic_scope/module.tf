e9393bb23302eb983686f02c8b50d916d922f8c9
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-07-16 14:13:30 +0200

    fix

--- modules/maintenance/assignment_dynamic_scope/module.tf
+++ modules/maintenance/assignment_dynamic_scope/module.tf
@@ -12,12 +12,22 @@ resource "azurerm_maintenance_assignment_dynamic_scope" "maintenance_assignment_
     #     for rg_key in var.local_combined_resources[var.settings.filter.resource_group_key][try(var.settings.filter.lz_key, var.client_config.landingzone_key)][rg_key].name
     #   ])
     # )
+    # resource_groups = try(
+    #   var.settings.filter.resource_group_name,
+    #   flatten([
+    #     for rg_key in var.settings.filter.resource_group_key : var.local_combined_resources[rg_key][try(var.settings.filter.lz_key, var.client_config.landingzone_key)].name
+    #   ])
+    # )
+
     resource_groups = try(
       var.settings.filter.resource_group_name,
       flatten([
-        for rg_key in var.settings.filter.resource_group_key : var.local_combined_resources[rg_key][try(var.settings.filter.lz_key, var.client_config.landingzone_key)].name
+        for rg_key in var.settings.filter.resource_group_key : [
+          for rg in var.local_combined_resources[rg_key][try(var.settings.filter.lz_key, var.client_config.landingzone_key)] : rg.name
+        ]
       ])
     )
+    
     resource_types  = try(var.settings.filter.resource_types, [])
     tag_filter      = try(var.settings.filter.tag_filter, null)
 
