54a77a16ae7795274fd4a8e8f1a4700fdabad715
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-07-17 16:35:05 +0200

    test

--- modules/maintenance/assignment_dynamic_scope/module.tf
+++ modules/maintenance/assignment_dynamic_scope/module.tf
@@ -2,26 +2,23 @@ resource "azurerm_maintenance_assignment_dynamic_scope" "maintenance_assignment_
   name                         = var.name
   maintenance_configuration_id = var.maintenance_configuration_id
 
-  dynamic "filter" {
-    for_each = { for key, value in try(var.settings.filter.resource_group, {}) : key => value }
-    content {
-      locations = try(filter.value.locations, [])
-      os_types  = try(filter.value.os_types, [])
-      resource_groups = try(flatten([
-        for key, value in var.resource_groups[filter.value.resource_group.key][try(filter.value.resource_group.lz_key, var.client_config.landingzone_key)] : value.id
-      ]), [])
-      resource_types = try(var.settings.filter.resource_types, [])
-      tag_filter     = try(var.settings.filter.tag_filter, null)
+  filter {
+    locations = try(filter.value.locations, [])
+    os_types  = try(filter.value.os_types, [])
+    resource_groups = try(flatten([
+      for key, value in var.resource_groups[filter.value.resource_group.key][try(filter.value.resource_group.lz_key, var.client_config.landingzone_key)] : value.id
+    ]), [])
+    resource_types = try(var.settings.filter.resource_types, [])
+    tag_filter     = try(var.settings.filter.tag_filter, null)
 
-      dynamic "tags" {
-        for_each = {
-          for key, value in try(var.settings.filter.tags, {}) : key => value
-        }
+    dynamic "tags" {
+      for_each = {
+        for key, value in try(var.settings.filter.tags, {}) : key => value
+      }
 
-        content {
-          tag    = tags.value.tag
-          values = tags.value.values
-        }
+      content {
+        tag    = tags.value.tag
+        values = tags.value.values
       }
     }
   }
