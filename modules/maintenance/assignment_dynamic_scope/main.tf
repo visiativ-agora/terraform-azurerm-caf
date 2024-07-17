724bbc05b6e2a6bbab569b52d6b91d63adf910da
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-07-17 14:20:08 +0200

    test

--- modules/maintenance/assignment_dynamic_scope/main.tf
+++ modules/maintenance/assignment_dynamic_scope/main.tf
@@ -24,15 +24,22 @@ locals {
   #   []
   # ))
 
-  resource_groups = flatten(
-    try([
-      for rg_key in try(var.settings.filter.resource_group_key, []) :
-      try(var.resource_groups[var.client_config.landingzone_key][rg_key], var.resource_groups[var.settings.filter.lz_key][rg_key], [])
-    ], []),
-    try([
-      for rg_key in try(var.settings.filter.resource_group.key, []) :
-      try(var.resource_groups[var.client_config.landingzone_key][rg_key], var.resource_groups[var.settings.filter.lz_key][rg_key], [])
-    ], []),
-    []
-  )
+  # resource_groups = coalesce(
+  #   try([
+  #     for rg_key in try(var.settings.filter.resource_group_key, []) :
+  #     try(var.resource_groups[var.client_config.landingzone_key][rg_key], var.resource_groups[var.settings.filter.lz_key][rg_key], [])
+  #   ], []),
+  #   try([
+  #     for rg_key in try(var.settings.filter.resource_group.key, []) :
+  #     try(var.resource_groups[var.client_config.landingzone_key][rg_key], var.resource_groups[var.settings.filter.lz_key][rg_key], [])
+  #   ], []),
+  #   []
+  # )
+
+  resource_groups = try(coalesce(
+    try(var.settings.filter.resource_group_key, null),
+    try(var.settings.filter.resource_group.key, null),
+    try(var.resource_groups[var.client_config.landingzone_key][var.settings.filter.resource_group_key], null),
+    try(var.resource_groups[var.client_config.landingzone_key][var.settings.filter.resource_group.key], null),
+  ), null)
 }
