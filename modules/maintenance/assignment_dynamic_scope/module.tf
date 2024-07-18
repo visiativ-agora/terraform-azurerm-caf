687dde0e7cd43958bb20186888ed98ab67098680
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-07-18 14:31:15 +0200

    fix

--- modules/maintenance/assignment_dynamic_scope/module.tf
+++ modules/maintenance/assignment_dynamic_scope/module.tf
@@ -3,12 +3,10 @@ resource "azurerm_maintenance_assignment_dynamic_scope" "maintenance_assignment_
   maintenance_configuration_id = var.maintenance_configuration_id
 
   filter {
-    locations = try(var.settings.locations, [])
-    os_types  = try(var.settings.os_types, [])
+    locations = try(var.settings.filter.locations, [])
+    os_types  = try(var.settings.filter.os_types, [])
 
-
-    # resource_groups = try(flatten([for rg_key in var.settings.filter.resource_group_key : var.resource_groups[var.client_config.landingzone_key][rg_key].name]), [])
-    resource_groups = try(flatten([for rg_key in var.settings.filter.resource_group_key : var.resource_groups[var.client_config.landingzone_key][rg_key].name]),try(flatten([for rg, rg_data in var.settings.filter.resources_groups : [for key in rg_data.key : var.resource_groups[var.client_config.landingzone_key][key].name]]), try(flatten([for rg, rg_data in var.settings.filter.resources_groups : [for key in rg_data.key : var.resource_groups[rg_data.lz_key][key].name]]), [])))
+    resource_groups = try(flatten([for rg_key in var.settings.filter.resource_group_key : var.resource_groups[var.client_config.landingzone_key][rg_key].name]), try(flatten([for rg, rg_data in var.settings.filter.resources_groups : [for key in rg_data.key : var.resource_groups[var.client_config.landingzone_key][key].name]]), try(flatten([for rg, rg_data in var.settings.filter.resources_groups : [for key in rg_data.key : var.resource_groups[rg_data.lz_key][key].name]]), [])))
 
     resource_types = try(var.settings.filter.resource_types, [])
     tag_filter     = try(var.settings.filter.tag_filter, null)
@@ -25,17 +23,3 @@ resource "azurerm_maintenance_assignment_dynamic_scope" "maintenance_assignment_
     }
   }
 }
-
-
-
-# resource "null_resource" "example" {
-
-#   provisioner "local-exec" {
-    
-#     # command = var.resource_groups[var.client_config.landingzone_key][var.settings.filter.resource_group_key[0]].name
-#     command = try(flatten([for rg_key in var.settings.filter.resource_group_key : var.resource_groups[var.client_config.landingzone_key][rg_key].name]), [])
-
-
-#   }
-
-# }
