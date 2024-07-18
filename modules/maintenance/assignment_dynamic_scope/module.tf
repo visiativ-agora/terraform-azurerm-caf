90462e391beb960b75e2738b98826eec43f07018
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-07-18 11:16:13 +0200

    test

--- modules/maintenance/assignment_dynamic_scope/module.tf
+++ modules/maintenance/assignment_dynamic_scope/module.tf
@@ -5,45 +5,10 @@ resource "azurerm_maintenance_assignment_dynamic_scope" "maintenance_assignment_
   filter {
     locations = try(var.settings.locations, [])
     os_types  = try(var.settings.os_types, [])
-    # resource_groups = try(flatten(
-    #   [
-    #   for key, value in var.resource_groups[try(var.settings.resource_group.lz_key, var.client_config.landingzone_key)][var.settings.resource_group.key].name
-    #   ]
-    # ), [])
-
-  #   resource_groups = flatten(
-  #   [
-  #     for key, value in try(var.resource_groups, {}) : [
-  #       can(value.id) ? value.id : var.resource_groups[try(value.lz_key, var.client_config.landingzone_key)][value.key]
-  #     ]
-  #   ]
-  # )
-
-    # resource_groups = try(flatten([
-    #   for key, value in var.resource_groups[try(var.settings.filter.lz_key, var.client_config.landingzone_key)] : value
-    #   if contains(var.settings.filter.resource_groups_keys, key)
-    #   ]), [])
-
-    # resource_groups = try(flatten([
-    #   for rg_key, rg_value in var.settings.filter.resources_groups :
-    #   [
-    #     for lz_key in rg_value.key :
-    #     if try(var.resource_groups[rg_value.lz_key][lz_key], null) != null :
-    #     var.resource_groups[rg_value.lz_key][lz_key]
-    #   ]
-    # ]), [])
-
-    # resource_groups = try(flatten([
-    #   for key, value in var.resource_groups[try(var.settings.filter.lz_key, var.client_config.landingzone_key)] : value
-    #   if contains(var.settings.filter.resource_groups_keys, key)
-    #   ]), [])
-
-    # resource_groups = try(flatten([
-    # for key, value in var.resource_groups[var.client_config.landingzone_key][var.settings.filter.resource_group_key] : value.name
-    # ]), [])
 
 
-    resource_groups = try(flatten([for rg_key in var.settings.filter.resource_group_key : var.resource_groups[var.client_config.landingzone_key][rg_key].name]), [])
+    # resource_groups = try(flatten([for rg_key in var.settings.filter.resource_group_key : var.resource_groups[var.client_config.landingzone_key][rg_key].name]), [])
+    resource_groups = try(flatten([for rg_key in var.settings.filter.resource_group_key : var.resource_groups[try(rg_key.lz_key, var.client_config.landingzone_key)][rg_key].name]), [])
 
     resource_types = try(var.settings.filter.resource_types, [])
     tag_filter     = try(var.settings.filter.tag_filter, null)
@@ -61,42 +26,16 @@ resource "azurerm_maintenance_assignment_dynamic_scope" "maintenance_assignment_
   }
 }
 
-#   filter {
-#     locations = try(var.settings.filter.locations, [])
-#     os_types  = try(var.settings.filter.os_types, [])
 
 
-#     resource_groups = local.resource_groups
+# resource "null_resource" "example" {
 
-#     # resource_groups = var.resource_groups
-
-
-#     resource_types = try(var.settings.filter.resource_types, [])
-#     tag_filter     = try(var.settings.filter.tag_filter, null)
-
-#     dynamic "tags" {
-#       for_each = {
-#         for key, value in try(var.settings.filter.tags, {}) : key => value
-#       }
-
-#       content {
-#         tag    = tags.value.tag
-#         values = tags.value.values
-#       }
-#     }
-#   }
-# }
-
-
-
-resource "null_resource" "example" {
-
-  provisioner "local-exec" {
+#   provisioner "local-exec" {
     
-    # command = var.resource_groups[var.client_config.landingzone_key][var.settings.filter.resource_group_key[0]].name
-    command = try(flatten([for rg_key in var.settings.filter.resource_group_key : var.resource_groups[var.client_config.landingzone_key][rg_key].name]), [])
+#     # command = var.resource_groups[var.client_config.landingzone_key][var.settings.filter.resource_group_key[0]].name
+#     command = try(flatten([for rg_key in var.settings.filter.resource_group_key : var.resource_groups[var.client_config.landingzone_key][rg_key].name]), [])
 
 
-  }
+#   }
 
-}
+# }
