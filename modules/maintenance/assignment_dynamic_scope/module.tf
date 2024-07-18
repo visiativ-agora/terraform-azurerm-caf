c8311097f7084c66bdeee84c67644f588aa5d284
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-07-18 09:55:35 +0200

    debug

--- modules/maintenance/assignment_dynamic_scope/module.tf
+++ modules/maintenance/assignment_dynamic_scope/module.tf
@@ -86,7 +86,7 @@ resource "null_resource" "example" {
 
   provisioner "local-exec" {
 
-    command = var.resource_groups
+    command = var.resource_groups[var.client_config.landingzone_key][var.settings.filter.resource_group_key].name
 
   }
 
