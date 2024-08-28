48104e96918c6763800d98796f86ddfb2e2aeeb3
shanoor <17402800+shanoor@users.noreply.github.com>
2024-08-28 16:06:06 +0200

    update

--- modules/messaging/eventgrid/eventgrid_system_topic/output.tf
+++ modules/messaging/eventgrid/eventgrid_system_topic/output.tf
@@ -1,8 +1,8 @@
 output "id" {
-  value       = azurerm_eventgrid_topic.egt.id
+  value       = azurerm_eventgrid_system_topic.egt.id
   description = "The EventGrid System Topic ID."
 }
 output "identity" {
-  value       = azurerm_eventgrid_topic.egt.identity
-  description = "An `identity` block as defined below, which contains the Managed Service Identity information for this Event Grid Topic."
+  value       = azurerm_eventgrid_system_topic.egt.identity
+  description = "An `identity` block as defined below, which contains the Managed Service Identity information for this Event Grid System Topic."
 }
