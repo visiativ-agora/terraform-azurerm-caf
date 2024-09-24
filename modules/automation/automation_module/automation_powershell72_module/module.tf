16f3dede18583199a120ec93f95f97e4f41b8500
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-09-24 16:42:57 +0200

    Add azurerm_automation_powershell72_module

---
+++ modules/automation/automation_module/automation_powershell72_module/module.tf
@@ -0,0 +1,19 @@
+resource "azurerm_automation_powershell72_module" "automation_powershell72_module" {
+  name                    = var.settings.name
+  automation_account_id   = var.automation_account_id  
+  tags                    = local.tags
+  
+  module_link {
+    uri  = var.settings.module_link.uri
+    hash = try(var.settings.module_link.hash, null)
+  }
+
+  dynamic "hash" {
+    for_each = try(var.settings.hash, null) == null ? [] : [1]
+
+    content {
+      algorithm = var.settings.hash.algorithm
+      value     = var.settings.hash.value
+    }
+  }
+}
