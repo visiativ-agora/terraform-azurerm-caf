164b9e2ed1694d3ded215149585eec3bae901fd4
shanoor <17402800+shanoor@users.noreply.github.com>
2024-10-09 12:07:43 +0200

    fix

--- modules/security/security_center/subscription_pricing/output.tf
+++ modules/security/security_center/subscription_pricing/output.tf
@@ -1,3 +1,3 @@
-output "vulnerability_assessment_resource_id" {
-  value = azurerm_security_center_server_vulnerability_assessment_virtual_machine.vuln.id
+output "id" {
+  value = azurerm_security_center_subscription_pricing.pricing.id
 }
