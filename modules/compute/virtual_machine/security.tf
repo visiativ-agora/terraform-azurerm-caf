c84fdf452b0ab4985e47794fc2701394cd14a152
shanoor <17402800+shanoor@users.noreply.github.com>
2024-10-09 10:57:27 +0200

    update

--- modules/compute/virtual_machine/security.tf
+++ modules/compute/virtual_machine/security.tf
@@ -2,12 +2,12 @@ module "vulnerability_assessment" {
   for_each = var.settings.security.enable_vulnerability_assessment ? var.settings.virtual_machine_settings : {}
 
   source             = "../../security/security_center/server_vulnerability_assessment"
-  virtual_machine_id = try(azurerm_virtual_machine.vm.id, azurerm_windows_virtual_machine.vm.id, azurerm_linux_virtual_machine.vm.id)
+  virtual_machine_id = try(azurerm_linux_virtual_machine.vm["linux"].id, azurerm_windows_virtual_machine.vm["windows"].id)
 }
 
 
 module "advanced_threat_protection" {
   source      = "../../security/security_center/advanced_threat_protection"
-  resource_id = try(azurerm_virtual_machine.vm.id, azurerm_windows_virtual_machine.vm.id, azurerm_linux_virtual_machine.vm.id)
+  resource_id = try(azurerm_linux_virtual_machine.vm["linux"].id, azurerm_windows_virtual_machine.vm["windows"].id)
   enabled     = try(var.settings.security.enable_advanced_threat_protection, false)
 }
