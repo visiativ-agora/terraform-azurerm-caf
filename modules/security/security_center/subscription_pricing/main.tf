001d61363a03c7c0931601c41d1ceacb15135af0
shanoor <17402800+shanoor@users.noreply.github.com>
2024-10-11 10:17:01 +0200

    added example

--- modules/security/security_center/subscription_pricing/main.tf
+++ modules/security/security_center/subscription_pricing/main.tf
@@ -1,22 +1,10 @@
 resource "azurerm_security_center_subscription_pricing" "pricing" {
-  tier          = var.tier
-  subplan       = try(var.subplan, null)
+  # Free or Standard
+  tier = var.tier
+  # Depends on the resource_type
+  subplan = try(var.subplan, null)
+  # can be one of: Api, AppServices, Arm, CloudPosture, ContainerRegistry, Containers, CosmosDbs, Dns, KeyVaults, KubernetesService, OpenSourceRelationalDatabases, SqlServers, SqlServerVirtualMachines, StorageAccounts, VirtualMachines
   resource_type = var.resource_type
-  # Api
-  # AppServices
-  # Arm
-  # CloudPosture
-  # ContainerRegistry
-  # Containers
-  # CosmosDbs
-  # Dns
-  # KeyVaults
-  # KubernetesService
-  # OpenSourceRelationalDatabases
-  # SqlServers
-  # SqlServerVirtualMachines
-  # StorageAccounts
-  # VirtualMachines
 
   # extensions list : https://learn.microsoft.com/en-us/rest/api/defenderforcloud/pricings/get?view=rest-defenderforcloud-2024-01-01&tabs=HTTP#extension
   dynamic "extension" {
