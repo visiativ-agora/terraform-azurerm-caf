cf13e75a8c4c5ab0d9f7183586417ea1b3fc78de
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-11-20 17:31:39 +0100

    fix location

--- cognitive_service.tf
+++ cognitive_service.tf
@@ -7,7 +7,7 @@ module "cognitive_services_account" {
   base_tags           = local.global_settings.inherit_tags
   resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
   resource_group_name = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].name
-  location            = lookup(each.value, "region", null) == null ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location : local.global_settings.regions[each.value.region]
+  location            = lookup(each.value, "region", null) != null ? each.value.region : can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
   settings            = each.value
   resource_groups     = local.combined_objects_resource_groups
   vnets               = local.combined_objects_networking
