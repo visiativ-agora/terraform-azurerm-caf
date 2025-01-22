3832e7c2bae296f7302bcc36b5f59cd085b91d0c
GitHub Action <action@github.com>
2025-01-22 15:05:36 +0000

    Apply terraform fmt

--- cdn_frontdoor.tf
+++ cdn_frontdoor.tf
@@ -1,6 +1,6 @@
 module "cdn_frontdoor_profiles" {
-  source              = "./modules/networking/cdn_frontdoor/cdn_frontdoor_profiles"
-  for_each            = try(local.networking.cdn_frontdoor_profiles, {})
+  source   = "./modules/networking/cdn_frontdoor/cdn_frontdoor_profiles"
+  for_each = try(local.networking.cdn_frontdoor_profiles, {})
 
   base_tags           = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
   global_settings     = local.global_settings
