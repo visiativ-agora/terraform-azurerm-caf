19b14d3527d8e882e8a90a61053834df5cbfa8de
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-02-10 17:56:25 +0100

    test

--- modules/databases/mssql_database/job_agent.tf
+++ modules/databases/mssql_database/job_agent.tf
@@ -246,7 +246,7 @@ resource "azapi_update_resource" "approve_private_endpoint" {
   count = try(var.settings.job.private_endpoint_name, null) == null ? 0 : 1
 
   type        = "Microsoft.Sql/servers/privateEndpointConnections@2024-05-01-preview"
-  resource_id = "${var.mssql_servers[try(var.settings.lz_key, var.client_config.landingzone_key)][var.settings.mssql_server_key].id}/privateEndpointConnections/${local.private_endpoint_connection_name}"
+  resource_id = "${var.mssql_servers[try(var.settings.lz_key, var.client_config.landingzone_key)][var.settings.mssql_server_key].id}/privateEndpointConnections/${try(local.private_endpoint_connection_name, "toto")}"
   # name = local.private_endpoint_connection_name
   body = jsonencode({
     properties = {
