c510d3fa119946fcbc2130b702ea6e453477b4d2
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-02-24 15:01:51 +0100

    debug

--- modules/backup_vault/backup_vault.tf
+++ modules/backup_vault/backup_vault.tf
@@ -50,9 +50,7 @@ resource "azapi_resource" "backup_vault" {
           } : null
           keyVaultProperties = {
             # keyUri = can(var.settings.backup_data_encryption.encryption_key) ? var.remote_objects.keyvault_keys[try(var.settings.backup_data_encryption.lz_key, var.client_config.landingzone_key)][var.settings.backup_data_encryption.keyvault_key_key].id : null
-            keyUri = try(
-              var.remote_objects.keyvault_keys[try(var.settings.backup_data_encryption.lz_key, var.client_config.landingzone_key)][var.settings.backup_data_encryption.keyvault_key_key].id, "toto"
-            )
+            keyUri = var.remote_objects.keyvault_keys[try(var.settings.backup_data_encryption.lz_key, var.client_config.landingzone_key)][var.settings.backup_data_encryption.keyvault_key_key].id            
           }          
           state = try(var.settings.backup_data_encryption.encryption_state, false) ? "Enabled" : "Disabled"
         }
