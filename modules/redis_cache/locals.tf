54e11ebee5e75d8a1ea858306d0d7ba646d87637
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-03-18 17:21:23 +0100

    fix

--- modules/redis_cache/locals.tf
+++ modules/redis_cache/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  redis_url = "rediss://:${azurerm_redis_cache.redis.primary_access_key}@${azurerm_redis_cache.redis.hostname}:@${azurerm_redis_cache.redis.ssl_port}"
+  redis_url = "rediss://:${azurerm_redis_cache.redis.primary_access_key}@${azurerm_redis_cache.redis.hostname}:${azurerm_redis_cache.redis.ssl_port}"
 }
