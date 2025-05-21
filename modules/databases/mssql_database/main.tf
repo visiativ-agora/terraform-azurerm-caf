c2bc63708302b70475beb59977c8847ef69c6ca8
Robin PICHON <robin.pichon@visiativ.com>
2025-05-21 17:36:42 +0200

    init

--- modules/databases/mssql_database/main.tf
+++ modules/databases/mssql_database/main.tf
@@ -28,6 +28,16 @@ locals {
         for lz_key, value in group.managed_identities : [
           for managed_identity_key in value.managed_identity_keys : [var.managed_identities[lz_key][managed_identity_key].name]
         ]
+        [for mi_key, mi_value in group : [
+          for value in try(mi_value.keys, mi_value.managed_identity_keys, []) : [
+            try(var.managed_identities[mi_value.lz_key][value].name, var.managed_identities[var.client_config.landingzone_key][value].name)
+          ]
+        ] if mi_key == "managed_identities"],
+        [for mi_key, mi_value in group : [
+          for value in mi_value.keys : [
+            try(var.azuread_groups[mi_value.lz_key][value].display_name, var.azuread_groups[var.client_config.landingzone_key][value].display_name)
+          ]
+        ] if mi_key == "azuread_groups"]
       ])
     }
   }
