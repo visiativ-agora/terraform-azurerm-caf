3ba106f9f01ab88498298f867b5e7ea73521d2c3
Robin PICHON <robin.pichon@visiativ.com>
2025-05-22 15:18:16 +0200

    push

--- modules/databases/mssql_database/scripts/set_db_permissions.sh
+++ modules/databases/mssql_database/scripts/set_db_permissions.sh
@@ -6,4 +6,4 @@ Driver = ODBC Driver 17 for SQL Server
 Server = tcp:${SQLCMDSERVER},1433
 Authentication = ActiveDirectoryMsi" | sudo tee /etc/odbc.ini > /dev/null
 
-sqlcmd -v DBUSERNAMES="${DBUSERNAMES}" DBROLES="${DBROLES}" -S "${SQLCMDSERVER}" -d "${SQLCMDDBNAME}" -i "${SQLFILEPATH}" -G
+sqlcmd -v DBUSERNAMES="${DBUSERNAMES}" DBROLES="${DBROLES}" -S "${SQLCMDSERVER}" -d "${SQLCMDDBNAME}" -i "${SQLFILEPATH}"
