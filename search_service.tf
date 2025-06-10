9256660ce8cdb0e28a77f941a818085b92081690
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-06-10 16:45:41 +0200

    fixed cosmosdnaccount

--- search_service.tf
+++ search_service.tf
@@ -42,23 +42,16 @@ module "search_shared_private_link_service" {
   global_settings   = local.global_settings
   settings          = each.value.shared_private_access
   search_service_id = module.search_service[each.value.search_service.key].id
-  # target_resource_id = {
-  #   "storage" = local.combined_objects_storage_accounts[
-  #     try(each.value.shared_private_access.target_resource.lz_key, local.client_config.landingzone_key)
-  #   ][each.value.shared_private_access.target_resource.key].id
-  #   "cosmosdb" = local.combined_objects_cosmosdb_sql_databases[
-  #     try(each.value.shared_private_access.target_resource.lz_key, local.client_config.landingzone_key)
-  #   ][each.value.shared_private_access.target_resource.key].id
-  # }[each.value.shared_private_access.target_resource.type]
   target_resource_id = {
     "storage" = local.combined_objects_storage_accounts[
       try(each.value.settings.target_resource.lz_key, local.client_config.landingzone_key)
     ][each.value.settings.target_resource.key].id
-    "cosmosdb" = local.combined_objects_cosmosdb_sql_databases[
+    "cosmosdbaccount" = local.combined_objects_cosmos_dbs[
       try(each.value.settings.target_resource.lz_key, local.client_config.landingzone_key)
     ][each.value.settings.target_resource.key].id
   }[each.value.settings.target_resource.type]  
 }
+
 
 output "search_shared_private_link_service" {
   value = module.search_shared_private_link_service
