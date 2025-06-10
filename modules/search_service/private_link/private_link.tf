6412bff0bca0579783f8125ad72542fccd946ab0
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-06-10 17:35:36 +0200

    request_message

--- modules/search_service/private_link/private_link.tf
+++ modules/search_service/private_link/private_link.tf
@@ -3,5 +3,5 @@ resource "azurerm_search_shared_private_link_service" "search_service_shared_pri
   search_service_id  = var.search_service_id
   subresource_name   = var.settings.subresource_name
   target_resource_id = var.target_resource_id
-  request_message    = try(var.settings.shared_private_link_service.request_message, null)
+  request_message    = try(var.settings.request_message, null)
 }
