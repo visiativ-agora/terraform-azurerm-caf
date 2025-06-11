76a27f2581e9d8acda01e2a1f72892689025a8bf
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-06-11 14:18:25 +0200

    fixed local shared_private_access

--- search_service.tf
+++ search_service.tf
@@ -22,16 +22,25 @@ output "search_service" {
 
 
 locals {
+  # shared_private_access = flatten([
+  #   for search_key, search_val in local.search_services.search_services : [
+  #     for spa_key, spa_val in search_val.shared_private_access : {
+  #       search_key = search_key
+  #       spa_key    = spa_key
+  #       settings   = spa_val
+  #     }
+  #   ]
+  # ])
+
   shared_private_access = flatten([
-    for search_key, search_val in local.search_services.search_services : [
+    for search_key, search_val in local.search_services.search_services : try([
       for spa_key, spa_val in search_val.shared_private_access : {
         search_key = search_key
         spa_key    = spa_key
         settings   = spa_val
       }
-    ]
+    ], [])
   ])
-
 
   target_resource_type_map = {
     "storage"         = "Microsoft.Storage/storageAccounts"
