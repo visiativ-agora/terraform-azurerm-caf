3f12e3a51f133a205ce89e61f16ea92742b5574d
paul trenchard <paul.trenchard@visiativ.com>
2025-06-19 15:26:49 +0200

    FIX: Correct resource type from azurerm_kubernetes_fleet_managers to azurerm_kubernetes_fleet_manager in fleet_manager.tf

--- modules/compute/aks_fleet_managers/fleet_manager.tf
+++ modules/compute/aks_fleet_managers/fleet_manager.tf
@@ -1,4 +1,4 @@
-resource "azurerm_kubernetes_fleet_managers" "kfm" {
+resource "azurerm_kubernetes_fleet_manager" "kfm" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
