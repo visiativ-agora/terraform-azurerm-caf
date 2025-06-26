2fd52d7da83789d9976bc491f183bea41e2861c9
paul trenchard <paul.trenchard@visiativ.com>
2025-06-26 11:32:36 +0200

    Refactor azurerm_kubernetes_fleet_member resource: update to use dynamic for_each and improve variable handling

--- modules/kubernetes_fleet_managers/kubernetes_fleet_members/aks_fleet_members.tf
+++ modules/kubernetes_fleet_managers/kubernetes_fleet_members/aks_fleet_members.tf
@@ -1,5 +1,19 @@
+# resource "azurerm_kubernetes_fleet_member" "kfme" {
+#   name = var.settings.name
+#   kubernetes_cluster_id = "/subscriptions/4936a57f-886a-4874-9ff3-bbbf940bde45/resourcegroups/aks-re1/providers/microsoft.containerservice/managedclusters/akscluster-re1-001"
+#   kubernetes_fleet_id = var.kubernetes_fleet_id
+# }
+
 resource "azurerm_kubernetes_fleet_member" "kfme" {
-  name = var.settings.name
-  kubernetes_cluster_id = "/subscriptions/4936a57f-886a-4874-9ff3-bbbf940bde45/resourcegroups/aks-re1/providers/microsoft.containerservice/managedclusters/akscluster-re1-001"
-  kubernetes_fleet_id = var.kubernetes_fleet_id
+  for_each = {
+    for member in var.members :
+    "${member.lz_key}.${member.keys}" => {
+      lz_key = member.lz_key
+      key    = member.keys
+    }
+  }
+
+  name                = each.value.key
+  kubernetes_fleet_id = data.azurerm_kubernetes_fleet_manager.kfm.id
+  kubernetes_cluster_id = module.landingzone[each.value.lz_key].resources.aks[each.value.key].id
 }
--- modules/kubernetes_fleet_managers/kubernetes_fleet_members/variables.tf
+++ modules/kubernetes_fleet_managers/kubernetes_fleet_members/variables.tf
@@ -11,3 +11,11 @@ variable "client_config" {
   description = "Client configuration object (see module README.md)."
 }
 variable "settings" {}
+
+variable "members" {
+  description = "Liste des membres à ajouter (lz_key + cluster key)"
+  type = list(object({
+    lz_key = string
+    keys   = string
+  }))
+}
