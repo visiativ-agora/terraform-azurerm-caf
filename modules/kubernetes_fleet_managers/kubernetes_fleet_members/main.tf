f6b8afd0c8dcd6198b2c9a198a0c104fa0ba4df6
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-07-30 11:23:35 +0200

    Refactor kubernetes_fleet_managers module: comment out unused module and output blocks for clarity

--- modules/kubernetes_fleet_managers/kubernetes_fleet_members/main.tf
+++ modules/kubernetes_fleet_managers/kubernetes_fleet_members/main.tf
@@ -16,21 +16,4 @@ locals {
   # resource_group_name = var.resource_group_name
 }
 
-locals {
-  fleet_members = flatten([
-    for fleet_key, fleet in aks_fleet_managers : [
-      for group_key, group in try(fleet.members_groups, {}) : [
-        for member_key, member in try(group.members, {}) : {
-          fleet_key  = fleet_key
-          fleet_name = fleet.name
-          group_key  = group_key
-          member_key = member_key
-          lz_key     = member.lz_key
-          key        = member.key
-          name       = try(member.name, member_key)
-        }
-      ]
-    ]
-  ])
-}
 
