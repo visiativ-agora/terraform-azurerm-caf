f01640be2e8b8753e78ac29845df10f7cc426d12
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-07-30 18:02:35 +0200

    fix

--- modules/kubernetes_fleet_managers/kubernetes_fleet_managers/aks_fleet_members.tf
+++ modules/kubernetes_fleet_managers/kubernetes_fleet_managers/aks_fleet_members.tf
@@ -1,7 +1,7 @@
 resource "azurerm_kubernetes_fleet_member" "kfme" {
   for_each = local.fleet_members
 
-  name                  = each.value
+  name                  = each.value.name
   kubernetes_cluster_id = var.kubernetes_cluster_id[each.value.lz_key][each.value.key].id
   kubernetes_fleet_id   = azurerm_kubernetes_fleet_manager.kfm.id
 }
--- modules/kubernetes_fleet_managers/kubernetes_fleet_managers/main.tf
+++ modules/kubernetes_fleet_managers/kubernetes_fleet_managers/main.tf
@@ -17,20 +17,42 @@ locals {
 }
 
 
+# locals {
+#   fleet_members = length(try(var.settings.members_groups, {})) > 0 ? merge(
+#     {
+#       for group_key, group in try(var.settings.members_groups, {}) :
+#       group_key => {
+#         for member_key, member in try(group.members, {}) :
+#         member_key => merge(
+#           member,
+#           {
+#             group_key   = group_key
+#             member_key  = member_key
+#           }
+#         )
+#       }
+#     }
+#   ) : {}
+# }
+
 locals {
-  fleet_members = length(try(var.settings.members_groups, {})) > 0 ? merge(
-    {
-      for group_key, group in try(var.settings.members_groups, {}) :
-      group_key => {
-        for member_key, member in try(group.members, {}) :
-        member_key => merge(
-          member,
-          {
-            group_key   = group_key
-            member_key  = member_key
-          }
-        )
-      }
-    }
-  ) : {}
+  members_groups_list = flatten([
+    for group_key, group in try(var.settings.members_groups, {}) : [
+      for member_key, member in try(group.members, {}) : merge(
+        member,
+        {
+          group_key  = group_key
+          member_key = member_key
+        }
+      )
+    ]
+  ])
+
+  fleet_members = length(local.members_groups_list) > 0 ? {
+      for idx, member in local.members_groups_list :
+      "${member.group_key}.${member.member_key}" => member
+    } : {}
 }
+
+
+
