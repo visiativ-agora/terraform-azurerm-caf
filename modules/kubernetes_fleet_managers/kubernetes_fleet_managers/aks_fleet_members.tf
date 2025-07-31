328b764433307d8e06ae18b227512cbd0a7a8eb6
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-07-31 10:26:40 +0200

    fix

--- modules/kubernetes_fleet_managers/kubernetes_fleet_managers/aks_fleet_members.tf
+++ modules/kubernetes_fleet_managers/kubernetes_fleet_managers/aks_fleet_members.tf
@@ -1,11 +1,10 @@
-# resource "azurerm_kubernetes_fleet_member" "kfme" {
-#   for_each = local.fleet_members
+resource "azurerm_kubernetes_fleet_member" "kfme" {
+  for_each = local.fleet_members
 
-#   name = each.value.name
-#   # kubernetes_cluster_id = var.kubernetes_cluster_id[each.value.lz_key][each.value.key].id
-#   kubernetes_cluster_id = try(var.kubernetes_cluster_id[each.value.lz_key][each.value.key].id, null)
-#   kubernetes_fleet_id   = azurerm_kubernetes_fleet_manager.kfm.id
-# }
+  name = each.value.name
+  kubernetes_cluster_id = var.kubernetes_cluster_id[each.value.lz_key][each.value.key].id
+  kubernetes_fleet_id   = azurerm_kubernetes_fleet_manager.kfm.id
+}
 
 
 resource "null_resource" "example" {
