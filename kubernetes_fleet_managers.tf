ae437c1e41e7bfee457fa1ed9a60746bdc671ee1
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-07-31 10:08:08 +0200

    debug

--- kubernetes_fleet_managers.tf
+++ kubernetes_fleet_managers.tf
@@ -3,14 +3,14 @@ module "kubernetes_fleet_managers" {
   source   = "./modules/kubernetes_fleet_managers/kubernetes_fleet_managers"
   for_each = local.kubernetes_fleet_managers.fleet_managers
 
-  client_config       = local.client_config
-  global_settings     = local.global_settings
-  settings            = each.value
-  location            = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
-  resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
+  client_config         = local.client_config
+  global_settings       = local.global_settings
+  settings              = each.value
+  location              = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
+  resource_group_name   = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
   kubernetes_cluster_id = local.combined_objects_aks_clusters
-  base_tags           = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
-  tags                = try(each.value.tags, {})
+  base_tags             = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
+  tags                  = try(each.value.tags, {})
 }
 
 output "kubernetes_fleet_managers" {
