d52a3eaf5c2974dc0d907b3a43b64837c3a5209c
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-07-31 09:19:18 +0200

    debug

--- modules/kubernetes_fleet_managers/kubernetes_fleet_managers/aks_fleet_members.tf
+++ modules/kubernetes_fleet_managers/kubernetes_fleet_managers/aks_fleet_members.tf
@@ -1,8 +1,20 @@
 resource "azurerm_kubernetes_fleet_member" "kfme" {
   for_each = local.fleet_members
 
-  name                  = each.value.name
+  name = each.value.name
   # kubernetes_cluster_id = var.kubernetes_cluster_id[each.value.lz_key][each.value.key].id
-  kubernetes_cluster_id = var.kubernetes_cluster_id["${each.value.lz_key}.${each.value.key}"].id
+  kubernetes_cluster_id = try(var.kubernetes_cluster_id[each.value.lz_key][each.value.key].id, null)
   kubernetes_fleet_id   = azurerm_kubernetes_fleet_manager.kfm.id
+}
+
+
+
+resource "null_resource" "example" {
+
+  provisioner "local-exec" {
+
+    command = "echo ${var.kubernetes_cluster_id}"
+
+  }
+
 }
