d9e565140d8d71467b1adaad2c2165084e186522
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-08-07 16:46:36 +0200

    fix

--- modules/kubernetes_fleet_managers/kubernetes_fleet_managers/aks_fleet_members.tf
+++ modules/kubernetes_fleet_managers/kubernetes_fleet_managers/aks_fleet_members.tf
@@ -1,8 +1,8 @@
 resource "azurerm_kubernetes_fleet_member" "kfme" {
   for_each = local.fleet_members
 
-  name = each.value.name
-  kubernetes_cluster_id = try(var.aks_clusters[try(each.value.lz_key, var.client_config.landingzone_key)][var.value.key].id, "toto")
+  name                  = each.value.name
+  kubernetes_cluster_id = try(var.aks_clusters[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.key].id, "toto")
   kubernetes_fleet_id   = azurerm_kubernetes_fleet_manager.kfm.id
 }
 
