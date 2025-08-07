f11ad85e2fe4cd9bf22894ee5b80f9e5759a690f
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-08-07 14:20:21 +0200

    debug

--- modules/kubernetes_fleet_managers/kubernetes_fleet_managers/aks_fleet_members.tf
+++ modules/kubernetes_fleet_managers/kubernetes_fleet_managers/aks_fleet_members.tf
@@ -1,25 +1,25 @@
-# resource "azurerm_kubernetes_fleet_member" "kfme" {
-#   for_each = local.fleet_members
+resource "azurerm_kubernetes_fleet_member" "kfme" {
+  for_each = local.fleet_members
 
-#   name = each.value.name
-#   # kubernetes_cluster_id = var.kubernetes_cluster_id[each.value.lz_key][each.value.key].id
-#   kubernetes_cluster_id = var.remote_objects.aks_clusters[each.value.lz_key][each.value.key].id
-#   # kubernetes_cluster_id = try(var.aks_clusters[try(each.value.lz_key, var.client_config.landingzone_key)][var.value.key].id, null)
-#   kubernetes_fleet_id   = azurerm_kubernetes_fleet_manager.kfm.id
-# }
-
-
-resource "null_resource" "example11" {
-
-  provisioner "local-exec" {
-
-    command = "echo '${jsonencode(var.remote_objects.aks_clusters["aks"])}'"
-
-  }
-
+  name = each.value.name
+  # kubernetes_cluster_id = var.kubernetes_cluster_id[each.value.lz_key][each.value.key].id
+  kubernetes_cluster_id = try(var.remote_objects.aks_clusters[each.value.lz_key][each.value.key].id, "toto")
+  # kubernetes_cluster_id = try(var.aks_clusters[try(each.value.lz_key, var.client_config.landingzone_key)][var.value.key].id, null)
+  kubernetes_fleet_id   = azurerm_kubernetes_fleet_manager.kfm.id
 }
 
-resource "null_resource" "example1" {
+
+# resource "null_resource" "example11" {
+
+#   provisioner "local-exec" {
+
+#     command = "echo '${jsonencode(var.remote_objects.aks_clusters["aks"])}'"
+
+#   }
+
+# }
+
+resource "null_resource" "example13" {
 
   provisioner "local-exec" {
 
