0466a5a7aba6ae6728d04ab179b500d6d52d4606
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-09-09 09:31:25 +0200

    feat: move function app host keys data block to module.tf and remove from locals.long_vars.tf

--- modules/webapps/linux_function_app/locals.long_vars.tf
+++ modules/webapps/linux_function_app/locals.long_vars.tf
@@ -5,8 +5,3 @@ locals {
   auth_settings_v2    = try(var.settings.auth_settings_v2, {})
   auth_settings       = try(var.settings.auth_settings, {})
 }
-
-data "azurerm_function_app_host_keys" "function_app_host_keys" {
-  name                = azurerm_linux_function_app.linux_function_app.name
-  resource_group_name = local.resource_group_name
-}
