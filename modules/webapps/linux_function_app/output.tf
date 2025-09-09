98d22d85651ab252f8b7adad51183a0a9413c4a6
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-09-09 09:09:08 +0200

    feat: add output for default and primary function keys in Linux Function App

--- modules/webapps/linux_function_app/output.tf
+++ modules/webapps/linux_function_app/output.tf
@@ -100,3 +100,12 @@ output "slot" {
     }
   }
 }
+
+output "default_function_key" {
+  value     = data.azurerm_function_app_host_keys.function_app_host_keys.default_function_key
+  sensitive = true
+}
+output "primary_key" {
+  value     = data.azurerm_function_app_host_keys.function_app_host_keys.primary_key
+  sensitive = true
+}
