c797a2bc5c207fb5f4080e7fc3c0f6541d674f16
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-09-22 17:07:01 +0200

    refactor: rename CDN Front Door profile module and output for consistency

--- modules/cdn/cdn_frontdoor_profile/cdn_cdn_frontdoor_profile.tf
+++ modules/cdn/cdn_frontdoor_profile/cdn_cdn_frontdoor_profile.tf
@@ -1,6 +1,6 @@
-module "cdn_frontdoor_profiles" {
+module "cdn_frontdoor_profile" {
   source   = "./modules/cdn/cdn_frontdoor_profile"
-  for_each = local.cdn.cdn_frontdoor_profiles
+  for_each = local.cdn.cdn_frontdoor_profile
 
   client_config   = local.client_config
   global_settings = local.global_settings
@@ -17,6 +17,6 @@ module "cdn_frontdoor_profiles" {
   }
 }
 
-output "cdn_frontdoor_profiles" {
-  value = module.cdn_frontdoor_profiles
+output "cdn_frontdoor_profile" {
+  value = module.cdn_frontdoor_profile
 }
