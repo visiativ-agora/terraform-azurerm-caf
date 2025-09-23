3b4d81d9ad4c8b7ff3e680fcfea2fb2d740d4e0a
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-09-23 16:53:53 +0200

    fix: update output for secret IDs in cdn_frontdoor_profile to support multiple secrets

--- modules/cdn/cdn_frontdoor_profile/secret/output.tf
+++ modules/cdn/cdn_frontdoor_profile/secret/output.tf
@@ -1,3 +1,7 @@
-output "secret_id" {
-  value = azurerm_cdn_frontdoor_secret.secret.id
+# output "secret_id" {
+#   value = azurerm_cdn_frontdoor_secret.secret.id
+# }
+
+output "secret_ids" {
+  value = { for k, v in azurerm_cdn_frontdoor_secret.secret : k => v.id }
 }
