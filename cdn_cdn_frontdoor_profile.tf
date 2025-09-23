b03f9164c2c3d4ce53ba1427ec77404f3186f97f
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-09-23 16:23:53 +0200

    fix: add keyvault_certificates to remote_objects and refactor azurerm_cdn_frontdoor_secret resource for dynamic handling

--- cdn_cdn_frontdoor_profile.tf
+++ cdn_cdn_frontdoor_profile.tf
@@ -12,6 +12,7 @@ module "cdn_frontdoor_profiles" {
   remote_objects = {
     diagnostics                   = local.combined_diagnostics
     keyvault_certificate_requests = local.combined_objects_keyvault_certificate_requests
+    keyvault_certificates         = local.combined_objects_keyvault_certificates
     managed_identities            = local.combined_objects_managed_identities
     storage_accounts              = local.combined_objects_storage_accounts
   }
