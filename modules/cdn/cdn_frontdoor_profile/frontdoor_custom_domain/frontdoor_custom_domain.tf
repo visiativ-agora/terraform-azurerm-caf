444b9fa91c90166fc8f7989e90ccea3aa9d48a27
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-09-25 09:17:21 +0200

    fix: update tls configuration in frontdoor_custom_domain to allow null values for certificate type and minimum tls version

--- modules/cdn/cdn_frontdoor_profile/frontdoor_custom_domain/frontdoor_custom_domain.tf
+++ modules/cdn/cdn_frontdoor_profile/frontdoor_custom_domain/frontdoor_custom_domain.tf
@@ -12,11 +12,13 @@ resource "azurerm_cdn_frontdoor_custom_domain" "custom_domain" {
   # pre_validated_cdn_frontdoor_custom_domain_id = try(var.settings.pre_validated_cdn_frontdoor_custom_domain_id, null)
 
   tls {
-    certificate_type = try(var.settings.tls.certificate_type, "ManagedCertificate")
+    certificate_type    = try(var.settings.tls.certificate_type, null)
+    minimum_tls_version = try(var.settings.tls.min_tls_version, null)
     cdn_frontdoor_secret_id = coalesce(
       try(var.settings.tls.cdn_frontdoor_secret_id, null),
       try(var.remote_objects.cdn_frontdoor_secrets[try(var.settings.tls.cdn_frontdoor_secret.lz_key, var.client_config.landingzone_key)][var.settings.tls.cdn_frontdoor_secret.key].id, null),
-      try(var.remote_objects.cdn_frontdoor_secrets[var.settings.tls.secret_key].id, null)
+      try(var.remote_objects.cdn_frontdoor_secrets[var.settings.tls.secret_key].id, null),
+      null
     )
   }
 
@@ -31,3 +33,4 @@ resource "azurerm_cdn_frontdoor_custom_domain" "custom_domain" {
     }
   }
 }
+
