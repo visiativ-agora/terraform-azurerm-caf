1c99b063af10412118d9c1732171580e6662c213
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-09-26 14:06:33 +0200

    fix: update app_service_logs handling in linux and windows function app modules

--- modules/webapps/linux_function_app/module.tf
+++ modules/webapps/linux_function_app/module.tf
@@ -58,8 +58,7 @@ resource "azurerm_linux_function_app" "linux_function_app" {
         use_custom_runtime          = lookup(local.site_config.application_stack, "use_custom_runtime", null)
       }
     }
-    dynamic "app_service_logs" {
-      # for_each = try(local.site_config.app_service_logs) != null ? [local.site_config.app_service_logs] : []
+    dynamic "app_service_logs" {      
       for_each = lookup(local.site_config, "app_service_logs", null) != null ? [local.site_config["app_service_logs"]] : []
 
       content {
--- modules/webapps/windows_function_app/module.tf
+++ modules/webapps/windows_function_app/module.tf
@@ -38,7 +38,7 @@ resource "azurerm_windows_function_app" "windows_function_app" {
       }
     }
     dynamic "app_service_logs" {
-      for_each = try(local.site_config.app_service_logs) != null ? [local.site_config.app_service_logs] : []
+      for_each = lookup(local.site_config, "app_service_logs", null) != null ? [local.site_config["app_service_logs"]] : []
       content {
         disk_quota_mb         = try(app_service_logs.value.disk_quota_mb, 35)
         retention_period_days = try(app_service_logs.value.retention_period_days, null)
