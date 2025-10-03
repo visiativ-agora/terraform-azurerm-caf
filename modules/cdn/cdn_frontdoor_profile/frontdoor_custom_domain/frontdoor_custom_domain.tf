51f021470a101af00f4b4223b6b367f1adc6363f
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-10-03 10:47:27 +0200

    fix: update DNS zone name and resource group name handling in frontdoor_custom_domain module for improved flexibility

--- modules/cdn/cdn_frontdoor_profile/frontdoor_custom_domain/frontdoor_custom_domain.tf
+++ modules/cdn/cdn_frontdoor_profile/frontdoor_custom_domain/frontdoor_custom_domain.tf
@@ -39,8 +39,8 @@ resource "azurerm_dns_txt_record" "b2c" {
   count = can(var.settings.dns_zone.key) ? 1 : 0
   
   name                = join(".", ["_dnsauth", local.subdomain_for_txt])
-  zone_name           = module.dns_zones[var.settings.dns_zone.key].name
-  resource_group_name = module.dns_zones[var.settings.dns_zone.key].resource_group_name
+  zone_name           = try(var.settings.dns_zone.lz_key, null) == null ? var.remote_objects.dns_zones[var.client_config.landingzone_key][var.settings.dns_zone.key].name : var.remote_objects.dns_zones[var.settings.dns_zone.lz_key][var.settings.dns_zone.key].name
+  resource_group_name = try(var.settings.dns_zone.lz_key, null) == null ? var.remote_objects.dns_zones[var.client_config.landingzone_key][var.settings.dns_zone.key].resource_group_name : var.remote_objects.dns_zones[var.settings.dns_zone.lz_key][var.settings.dns_zone.key].resource_group_name
   ttl                 = 60
 
   record {
