1aa29cab02d935f8123c9d2a0a2796b5b3cebff1
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-10-06 11:00:53 +0200

    Add dynamic role assignment for Redis Cache access policies

--- redis_caches.tf
+++ redis_caches.tf
@@ -9,6 +9,7 @@ module "redis_caches" {
   diagnostics         = local.combined_diagnostics
   global_settings     = local.global_settings
   redis               = each.value.redis
+  redis_role_assignment = try(each.value.redis.redis_role_assignment, {})
   subnet_id           = can(each.value.subnet_id) || can(each.value.subnet_key) == false ? try(each.value.subnet_id, null) : local.combined_objects_networking[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
   tags                = try(each.value.tags, null)
   private_endpoints   = try(each.value.private_endpoints, {})
