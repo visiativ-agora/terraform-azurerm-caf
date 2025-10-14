88ea021ea9ad6976456c93a1d256bea8199b3b65
GitHub Action <action@github.com>
2025-10-14 10:15:52 +0000

    Apply terraform fmt

--- roles.tf
+++ roles.tf
@@ -226,15 +226,15 @@ locals {
   roles_to_process = {
     for mapping in
     flatten(
-      [                                                                                                   # Variable
-        for key_mode, all_role_mapping in var.role_mapping : [                                            #  built_in_role_mapping = {
-          for key, role_mappings in all_role_mapping : [                                                  #       aks_clusters = {
-            for scope_key_resource, role_mapping in role_mappings : [                                     #         seacluster = {
-              for role_definition_name, resources in role_mapping : [                                     #           "Azure Kubernetes Service Cluster Admin Role" = {
-                for object_id_key, object_resources in resources : [                                      #             azuread_group_keys = {
-                  for object_id_key_resource in try(object_resources.keys, []) :                          #               keys = [ "aks_admins" ] ----End of variable
-                  {                                                                                       # "seacluster_Azure_Kubernetes_Service_Cluster_Admin_Role_aks_admins" = {
-                    mode                    = key_mode                                                    #   "mode" = "built_in_role_mapping"
+      [                                                                          # Variable
+        for key_mode, all_role_mapping in var.role_mapping : [                   #  built_in_role_mapping = {
+          for key, role_mappings in all_role_mapping : [                         #       aks_clusters = {
+            for scope_key_resource, role_mapping in role_mappings : [            #         seacluster = {
+              for role_definition_name, resources in role_mapping : [            #           "Azure Kubernetes Service Cluster Admin Role" = {
+                for object_id_key, object_resources in resources : [             #             azuread_group_keys = {
+                  for object_id_key_resource in try(object_resources.keys, []) : #               keys = [ "aks_admins" ] ----End of variable
+                  {                                                              # "seacluster_Azure_Kubernetes_Service_Cluster_Admin_Role_aks_admins" = {
+                    mode                    = key_mode                           #   "mode" = "built_in_role_mapping"
                     scope_resource_key      = key
                     scope_lz_key            = try(role_mapping.lz_key, null)
                     role_lz_key             = try(resources.lz_key, null)
