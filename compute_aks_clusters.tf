11a63ffb7f159944939bde87a76c295a8e83db66
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-10-29 09:49:35 +0100

    Add fleet manager configuration to AKS clusters module

--- compute_aks_clusters.tf
+++ compute_aks_clusters.tf
@@ -1,3 +1,4 @@
+
 output "aks_clusters" {
   value = module.aks_clusters
 }
@@ -10,7 +11,10 @@ module "aks_clusters" {
   client_config        = local.client_config
   diagnostic_profiles  = try(each.value.diagnostic_profiles, {})
   diagnostics          = local.combined_diagnostics
-  global_settings      = local.global_settings
+  global_settings      = local.global_settings  
+  
+  fleet_manager        = try(local.combined_objects_kubernetes_fleet_managers[try(each.value.fleet_manager.lz_key,local.client_config.landingzone_key)][try(each.value.fleet_manager_key, each.value.fleet_manager.key)], null)
+
   managed_identities   = local.combined_objects_managed_identities
   settings             = each.value
   vnets                = local.combined_objects_networking
