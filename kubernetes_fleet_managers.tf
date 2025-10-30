6fa6190ae5abb91dbe5c3e53c3d914b271bf5c13
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-10-30 17:08:41 +0100

    Add diagnostics and diagnostic profiles to Kubernetes Fleet Manager module

--- kubernetes_fleet_managers.tf
+++ kubernetes_fleet_managers.tf
@@ -5,6 +5,8 @@ module "kubernetes_fleet_managers" {
 
   client_config       = local.client_config
   global_settings     = local.global_settings
+  diagnostic_profiles = try(each.value.diagnostic_profiles, {})
+  diagnostics         = local.combined_diagnostics  
   settings            = each.value
   location            = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
