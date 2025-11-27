1ecaa19f555d44f16c4066bf309026d24b0b6e7b
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-11-27 09:33:31 +0100

    Refactor DNS TXT record name assignment and enhance subdomain extraction logic

--- modules/cdn/cdn_frontdoor_profile/frontdoor_custom_domain/main.tf
+++ modules/cdn/cdn_frontdoor_profile/frontdoor_custom_domain/main.tf
@@ -6,7 +6,18 @@ terraform {
     }
   }
 }
+# locals {
+#   domain_parts      = split(".", var.settings.host_name)
+#   subdomain_for_txt = local.domain_parts[0]
+# }
+
 locals {
-  domain_parts      = split(".", var.settings.host_name)
-  subdomain_for_txt = local.domain_parts[0]
+  # Calcul automatique du sous-domaine par soustraction
+  subdomain_for_txt = trim(
+    replace(
+      replace(var.settings.host_name, "${var.remote_objects.dns_zones[try(var.settings.dns_zone.lz_key, var.client_config.landingzone_key)][var.settings.dns_zone.key].name}.", ""),
+      ".", "", # Enlève le dernier point s'il reste
+    ),
+    "."
+  )
 }
