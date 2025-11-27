f7d33ca031106c8387b5b14a54f67d4ff4c05cd2
Kevin Dlm <133667252+kevindelmont@users.noreply.github.com>
2025-11-27 14:16:17 +0100

    Fix front door txt dns zone (#113)
    * Refactor DNS TXT record name assignment and enhance subdomain extraction logic

--- modules/cdn/cdn_frontdoor_profile/frontdoor_custom_domain/frontdoor_custom_domain.tf
+++ modules/cdn/cdn_frontdoor_profile/frontdoor_custom_domain/frontdoor_custom_domain.tf
@@ -33,12 +33,11 @@ resource "azurerm_cdn_frontdoor_custom_domain" "custom_domain" {
     }
   }
 }
-
 resource "azurerm_dns_txt_record" "b2c" {
   count = can(var.settings.dns_zone.key) ? 1 : 0
 
-  name                = join(".", ["_dnsauth", local.subdomain_for_txt])
-  zone_name           = try(var.settings.dns_zone.lz_key, null) == null ? var.remote_objects.dns_zones[var.client_config.landingzone_key][var.settings.dns_zone.key].name : var.remote_objects.dns_zones[var.settings.dns_zone.lz_key][var.settings.dns_zone.key].name
+  name                = "_dnsauth.${local.subdomain_for_txt}"
+  zone_name           = local.zone_name
   resource_group_name = try(var.settings.dns_zone.lz_key, null) == null ? var.remote_objects.dns_zones[var.client_config.landingzone_key][var.settings.dns_zone.key].resource_group_name : var.remote_objects.dns_zones[var.settings.dns_zone.lz_key][var.settings.dns_zone.key].resource_group_name
   ttl                 = 60
 
--- modules/cdn/cdn_frontdoor_profile/frontdoor_custom_domain/main.tf
+++ modules/cdn/cdn_frontdoor_profile/frontdoor_custom_domain/main.tf
@@ -7,6 +7,8 @@ terraform {
   }
 }
 locals {
-  domain_parts      = split(".", var.settings.host_name)
-  subdomain_for_txt = local.domain_parts[0]
+  zone_name = try(var.settings.dns_zone.lz_key, null) == null ? var.remote_objects.dns_zones[var.client_config.landingzone_key][var.settings.dns_zone.key].name : var.remote_objects.dns_zones[var.settings.dns_zone.lz_key][var.settings.dns_zone.key].name
+  subdomain_for_txt = trim(
+    replace(var.settings.host_name, ".${local.zone_name}", ""), "."
+  )
 }
