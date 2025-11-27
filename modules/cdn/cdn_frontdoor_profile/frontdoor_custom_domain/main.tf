e1e758d7cdd51eb7676023f9110393bd25ed2a1d
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-11-27 14:15:14 +0100

    Remove commented-out DNS TXT record configuration and clean up local variable definitions

--- modules/cdn/cdn_frontdoor_profile/frontdoor_custom_domain/main.tf
+++ modules/cdn/cdn_frontdoor_profile/frontdoor_custom_domain/main.tf
@@ -6,17 +6,8 @@ terraform {
     }
   }
 }
-# locals {
-#   domain_parts      = split(".", var.settings.host_name)
-#   subdomain_for_txt = local.domain_parts[0]
-# }
-
-
 locals {
-  # Récupère la zone DNS exacte
   zone_name = try(var.settings.dns_zone.lz_key, null) == null ? var.remote_objects.dns_zones[var.client_config.landingzone_key][var.settings.dns_zone.key].name : var.remote_objects.dns_zones[var.settings.dns_zone.lz_key][var.settings.dns_zone.key].name
-
-  # Soustrait la zone du hostname pour garder le sous-domaine
   subdomain_for_txt = trim(
     replace(var.settings.host_name, ".${local.zone_name}", ""), "."
   )
