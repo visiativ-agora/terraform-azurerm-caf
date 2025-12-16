7aba4ef57801f6c2d933e8f799f1e8bd0f18cfe9
Kévin DELMONT <kevin.delmont@visiativ.com>
2025-12-16 11:05:34 +0100

    Fix resource type in container app job module

--- modules/compute/container_app_job/container_app_job.tf
+++ modules/compute/container_app_job/container_app_job.tf
@@ -1,7 +1,7 @@
 resource "azurecaf_name" "caj" {
   name          = var.settings.name
   prefixes      = var.global_settings.prefixes
-  resource_type = "azurerm_container_app_job"
+  resource_type = "azurerm_container_app"
   random_length = var.global_settings.random_length
   clean_input   = true
   passthrough   = var.global_settings.passthrough
