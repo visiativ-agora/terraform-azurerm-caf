d0b3e619cb11b6a981e667da9e1801209a17e6a6
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-02-05 10:05:53 +0100

    Set default value for private_endpoint_network_policies in subnet module

--- modules/networking/virtual_network/subnet/subnet.tf
+++ modules/networking/virtual_network/subnet/subnet.tf
@@ -17,7 +17,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes                              = var.address_prefixes
   service_endpoints                             = var.service_endpoints
   service_endpoint_policy_ids                   = local.service_endpoint_policy_ids
-  private_endpoint_network_policies             = try(var.private_endpoint_network_policies, null)
+  private_endpoint_network_policies             = try(var.private_endpoint_network_policies, "Disabled")
   private_link_service_network_policies_enabled = try(var.private_link_service_network_policies_enabled, null)
   default_outbound_access_enabled               = try(var.default_outbound_access_enabled, false)
 
