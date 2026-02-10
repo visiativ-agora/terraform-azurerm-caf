a37b09d931de3d7a286d1d9e3a9e2a02086e7f8d
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-02-10 16:43:50 +0100

    Add tags support to Key Vault secret resource

--- modules/security/dynamic_keyvault_secrets/secret_immutable/keyvault_secret.tf
+++ modules/security/dynamic_keyvault_secrets/secret_immutable/keyvault_secret.tf
@@ -3,6 +3,7 @@ resource "azurerm_key_vault_secret" "secret" {
   name         = var.name
   value        = var.value
   key_vault_id = var.keyvault_id
+  tags         = var.tags
 
   lifecycle {
     ignore_changes = [
