a637fe5bb652a23348628df64ecba6246c5ed1b1
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-02-11 11:17:43 +0100

    Refactor PostgreSQL flexible server locals to streamline user role assignments and improve conditional checks for managed identities, Azure AD groups, and users

--- modules/databases/postgresql_flexible_server/locals.tf
+++ modules/databases/postgresql_flexible_server/locals.tf
@@ -28,41 +28,41 @@ locals {
   db_permissions = {
     for db_key, db in try(var.settings.postgresql_databases, {}) : db_key => {
       db_name = db.name
-      users = compact(flatten(concat(
+      users = concat(
         # Managed Identities
-        # La structure est: managed_identities = { keys = [ { lz_key = "...", key = "...", role_type = "..." } ] }
         [
           for mi_config in try(db.managed_identities.keys, []) :
-          can(var.remote_objects.managed_identities[try(mi_config.lz_key, var.client_config.landingzone_key)][mi_config.key]) ? {
+          {
             username      = var.remote_objects.managed_identities[try(mi_config.lz_key, var.client_config.landingzone_key)][mi_config.key].name
             object_id     = var.remote_objects.managed_identities[try(mi_config.lz_key, var.client_config.landingzone_key)][mi_config.key].principal_id
             role_type     = try(mi_config.role_type, "readwrite")
             custom_grants = try(mi_config.custom_grants, "")
-          } : null
+          }
+          if can(var.remote_objects.managed_identities[try(mi_config.lz_key, var.client_config.landingzone_key)][mi_config.key])
         ],
         # Azure AD Groups
-        # La structure attendue: azuread_groups = { keys = [ { lz_key = "...", key = "...", role_type = "..." } ] }
         [
           for grp_config in try(db.azuread_groups.keys, []) :
-          can(var.remote_objects.azuread_groups[try(grp_config.lz_key, var.client_config.landingzone_key)][grp_config.key]) ? {
+          {
             username      = var.remote_objects.azuread_groups[try(grp_config.lz_key, var.client_config.landingzone_key)][grp_config.key].display_name
             object_id     = var.remote_objects.azuread_groups[try(grp_config.lz_key, var.client_config.landingzone_key)][grp_config.key].object_id
             role_type     = try(grp_config.role_type, "readwrite")
             custom_grants = try(grp_config.custom_grants, "")
-          } : null
+          }
+          if can(var.remote_objects.azuread_groups[try(grp_config.lz_key, var.client_config.landingzone_key)][grp_config.key])
         ],
         # Azure AD Users
-        # La structure attendue: azuread_users = { keys = [ { lz_key = "...", key = "...", role_type = "..." } ] }
         [
           for usr_config in try(db.azuread_users.keys, []) :
-          can(var.remote_objects.azuread_users[try(usr_config.lz_key, var.client_config.landingzone_key)][usr_config.key]) ? {
+          {
             username      = var.remote_objects.azuread_users[try(usr_config.lz_key, var.client_config.landingzone_key)][usr_config.key].user_principal_name
             object_id     = var.remote_objects.azuread_users[try(usr_config.lz_key, var.client_config.landingzone_key)][usr_config.key].object_id
             role_type     = try(usr_config.role_type, "readonly")
             custom_grants = try(usr_config.custom_grants, "")
-          } : null
+          }
+          if can(var.remote_objects.azuread_users[try(usr_config.lz_key, var.client_config.landingzone_key)][usr_config.key])
         ]
-      )))
+      )
     } if (
       length(try(db.managed_identities.keys, [])) > 0 ||
       length(try(db.azuread_groups.keys, [])) > 0 ||
