0d38eb8cb771ae54befbc4f4d934629e284998ea
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-02-13 11:03:04 +0100

    Added express_vulnerability_assessment_enabled on mssql server

--- modules/databases/mssql_server/server.tf
+++ modules/databases/mssql_server/server.tf
@@ -1,14 +1,15 @@
 resource "azurerm_mssql_server" "mssql" {
-  name                          = azurecaf_name.mssql.result
-  resource_group_name           = local.resource_group_name
-  location                      = local.location
-  version                       = try(var.settings.version, "12.0")
-  administrator_login           = try(var.settings.azuread_administrator.azuread_authentication_only, false) == true ? null : var.settings.administrator_login
-  administrator_login_password  = try(var.settings.azuread_administrator.azuread_authentication_only, false) == true ? null : try(var.settings.administrator_login_password, azurerm_key_vault_secret.sql_admin_password[0].value)
-  public_network_access_enabled = try(var.settings.public_network_access_enabled, false)
-  connection_policy             = try(var.settings.connection_policy, null)
-  minimum_tls_version           = try(var.settings.minimum_tls_version, "1.2")
-  tags                          = local.tags
+  name                                     = azurecaf_name.mssql.result
+  resource_group_name                      = local.resource_group_name
+  location                                 = local.location
+  version                                  = try(var.settings.version, "12.0")
+  administrator_login                      = try(var.settings.azuread_administrator.azuread_authentication_only, false) == true ? null : var.settings.administrator_login
+  administrator_login_password             = try(var.settings.azuread_administrator.azuread_authentication_only, false) == true ? null : try(var.settings.administrator_login_password, azurerm_key_vault_secret.sql_admin_password[0].value)
+  public_network_access_enabled            = try(var.settings.public_network_access_enabled, false)
+  connection_policy                        = try(var.settings.connection_policy, null)
+  minimum_tls_version                      = try(var.settings.minimum_tls_version, "1.2")
+  express_vulnerability_assessment_enabled = try(var.settings.express_vulnerability_assessment_enabled, false)
+  tags                                     = local.tags
 
   dynamic "azuread_administrator" {
     for_each = can(var.settings.azuread_administrator) ? [var.settings.azuread_administrator] : []
