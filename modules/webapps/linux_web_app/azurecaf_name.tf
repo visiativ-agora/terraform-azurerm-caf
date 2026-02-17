bb18cf517b58973aa05586e9cf319d5a9b9b12de
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-02-17 17:35:22 +0100

    Refactor azurecaf_name resources in web apps module for improved clarity and consistency

--- modules/webapps/linux_web_app/azurecaf_name.tf
+++ modules/webapps/linux_web_app/azurecaf_name.tf
@@ -1,9 +1,10 @@
 resource "azurecaf_name" "linux_web_app" {
   name          = var.settings.name
   resource_type = "azurerm_linux_web_app"
-  prefixes      = var.global_settings.prefixes
-  suffixes      = var.global_settings.suffixes
-  use_slug      = var.global_settings.use_slug
+  # prefixes      = var.global_settings.prefixes
+  # suffixes      = var.global_settings.suffixes
+  # use_slug      = var.global_settings.use_slug
   clean_input   = true
-  separator     = "-"
+  passthrough = true
+  # separator     = "-"
 }
--- modules/webapps/service_plan/azure_caf_name.tf
+++ modules/webapps/service_plan/azure_caf_name.tf
@@ -1,9 +1,10 @@
 resource "azurecaf_name" "plan" {
   name          = var.settings.name
   resource_type = "azurerm_app_service_plan"
-  prefixes      = var.global_settings.prefixes
-  random_length = var.global_settings.random_length
+  # prefixes      = var.global_settings.prefixes
+  # random_length = var.global_settings.random_length
   clean_input   = true
-  passthrough   = var.global_settings.passthrough
-  use_slug      = var.global_settings.use_slug
+  passthrough = true
+  # passthrough   = var.global_settings.passthrough
+  # use_slug      = var.global_settings.use_slug
 }
