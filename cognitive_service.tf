15c9a15b0a6ea69dd12a640f19e8b9a2372cdd80
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-02-17 09:45:06 +0100

    Refactor location handling in cognitive services account module for improved resource group resolution

--- cognitive_service.tf
+++ cognitive_service.tf
@@ -4,7 +4,7 @@ module "cognitive_services_account" {
   client_config       = local.client_config
   global_settings     = local.global_settings
   settings            = each.value
-  location            = try(each.value.location, null)
+  location            = lookup(each.value, "region", null) == null ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location : local.global_settings.regions[each.value.region]
   base_tags           = local.global_settings.inherit_tags
   resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
   resource_group_name = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].name
