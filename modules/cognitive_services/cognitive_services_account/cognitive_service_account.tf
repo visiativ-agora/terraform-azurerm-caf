d03747a658fc3dc1ae9d56380082c0f96b09817d
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-02-17 10:01:34 +0100

    Refactor network ACLs handling in cognitive account module for improved clarity and migration support

--- modules/cognitive_services/cognitive_services_account/cognitive_service_account.tf
+++ modules/cognitive_services/cognitive_services_account/cognitive_service_account.tf
@@ -45,6 +45,7 @@ resource "azurerm_cognitive_account" "service" {
   metrics_advisor_aad_client_id   = (var.settings.kind == "MetricsAdvisor" || var.settings.kind == "QnAMaker") ? try(var.settings.metrics_advisor_aad_client_id, null) : null
   metrics_advisor_super_user_name = (var.settings.kind == "MetricsAdvisor" || var.settings.kind == "QnAMaker") ? try(var.settings.metrics_advisor_super_user_name, null) : null
   metrics_advisor_website_name    = (var.settings.kind == "MetricsAdvisor" || var.settings.kind == "QnAMaker") ? try(var.settings.metrics_advisor_website_name, null) : null
+  
   dynamic "network_acls" {
     for_each = can(var.settings.network_acls) ? [var.settings.network_acls] : []
     content {
@@ -54,27 +55,28 @@ resource "azurerm_cognitive_account" "service" {
       dynamic "virtual_network_rules" {
         for_each = try(network_acls.value.subnets, {})
         content {
-          subnet_id                            = can(virtual_network_rules.value.subnet_id) || can(virtual_network_rules.value.virtual_subnet_key) ? try(virtual_network_rules.value.subnet_id, var.virtual_subnets[try(virtual_network_rules.value.lz_key, var.client_config.landingzone_key)][virtual_network_rules.value.virtual_subnet_key].id) : var.vnets[try(virtual_network_rules.value.lz_key, var.client_config.landingzone_key)][virtual_network_rules.value.vnet_key].subnets[virtual_network_rules.value.subnet_key].id
+          subnet_id                            = can(virtual_network_rules.value.subnet_id) || can(virtual_network_rules.value.virtual_subnet_key) ? try(virtual_network_rules.value.subnet_id, var.remote_objects.virtual_subnets[try(virtual_network_rules.value.lz_key, var.client_config.landingzone_key)][virtual_network_rules.value.virtual_subnet_key].id) : var.remote_objects.vnets[try(virtual_network_rules.value.lz_key, var.client_config.landingzone_key)][virtual_network_rules.value.vnet_key].subnets[virtual_network_rules.value.subnet_key].id
           ignore_missing_vnet_service_endpoint = try(virtual_network_rules.value.ignore_missing_vnet_service_endpoint, null)
         }
       }
 
-      # to support migration from 2.99.0 to 3.7.0
-      dynamic "virtual_network_rules" {
-        for_each = can(network_acls.value.virtual_network_subnet_ids) ? toset(network_acls.value.virtual_network_subnet_ids) : []
+        # # to support migration from 2.99.0 to 3.7.0
+        # dynamic "virtual_network_rules" {
+        #   for_each = can(network_acls.value.virtual_network_subnet_ids) ? toset(network_acls.value.virtual_network_subnet_ids) : []
 
-        content {
-          subnet_id = virtual_network_rules.value
-        }
-      }
+        #   content {
+        #     subnet_id = virtual_network_rules.value
+        #   }
+        # }
 
-      dynamic "virtual_network_rules" {
-        for_each = try(network_acls.value.virtual_network_rules, {})
+        # dynamic "virtual_network_rules" {
+        #   for_each = try(network_acls.value.virtual_network_rules, {})
 
-        content {
-          subnet_id                            = virtual_network_rules.value.subnet_id
-          ignore_missing_vnet_service_endpoint = try(virtual_network_rules.value.ignore_missing_vnet_service_endpoint, null)
-        }
+        #   content {
+        #     subnet_id                            = virtual_network_rules.value.subnet_id
+        #     ignore_missing_vnet_service_endpoint = try(virtual_network_rules.value.ignore_missing_vnet_service_endpoint, null)
+        #   }
+        # }
       }
     }
   }
