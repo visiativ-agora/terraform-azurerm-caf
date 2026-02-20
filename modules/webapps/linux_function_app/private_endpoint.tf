0edde5697715a44dd27a1989a10b6cacc4b28423
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-02-20 11:27:08 +0100

    Fix indentation and formatting in private_endpoint module for consistency

--- modules/webapps/linux_function_app/private_endpoint.tf
+++ modules/webapps/linux_function_app/private_endpoint.tf
@@ -6,12 +6,11 @@ module "private_endpoint" {
   name                = each.value.name
   location            = local.location
   resource_group_name = local.resource_group_name
-  #subnet_id           = can(each.value.subnet_id) ? each.value.subnet_id : var.remote_objects.vnets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
-  subnet_id       = can(each.value.subnet_id) || can(each.value.subnet_key) ? try(each.value.subnet_id, var.remote_objects.virtual_subnets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.subnet_key].id) : var.remote_objects.vnets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
-  settings        = each.value
-  global_settings = var.global_settings
-  tags            = local.tags
-  base_tags       = var.base_tags
-  private_dns     = var.remote_objects.private_dns
-  client_config   = var.client_config
+  subnet_id           = can(each.value.subnet_id) ? each.value.subnet_id : var.remote_objects.vnets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
+  settings            = each.value
+  global_settings     = var.global_settings
+  tags                = local.tags
+  base_tags           = var.base_tags
+  private_dns         = var.remote_objects.private_dns
+  client_config       = var.client_config
 }
