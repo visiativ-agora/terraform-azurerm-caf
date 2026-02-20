3f33c896432c42000822193723d1ef7a1785881c
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-02-20 10:57:25 +0100

    Refactor dynamic app settings retrieval for improved key handling and clarity

--- webapps_linux_function_app.tf
+++ webapps_linux_function_app.tf
@@ -13,7 +13,6 @@ module "linux_function_apps" {
 
   remote_objects = {
     service_plans        = local.combined_objects_service_plans
-    service_plans        = local.combined_objects_service_plans
     combined_objects     = local.dynamic_app_settings_combined_objects
     diagnostics          = local.combined_diagnostics
     keyvaults            = local.combined_objects_keyvaults
@@ -23,7 +22,6 @@ module "linux_function_apps" {
     managed_identities   = local.combined_objects_managed_identities
     private_dns          = local.combined_objects_private_dns
     application_insights = local.combined_objects_application_insights
-
   }
 }
 
