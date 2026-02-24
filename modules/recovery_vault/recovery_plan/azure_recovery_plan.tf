3fe649cfc392f03a585fea690d8ffd2fed9839b0
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-02-24 11:06:42 +0100

    Fix recovery plan

--- modules/recovery_vault/recovery_plan/azure_recovery_plan.tf
+++ modules/recovery_vault/recovery_plan/azure_recovery_plan.tf
@@ -7,16 +7,18 @@ resource "azurerm_site_recovery_replication_recovery_plan" "replication_plan" {
 
 
   dynamic "shutdown_recovery_group" {
-    for_each = try(var.settings.shutdown_recovery_group, null) != null ? [var.settings.shutdown_recovery_group] : []
+    for_each = [1]
     content {
       dynamic "pre_action" {
-        for_each = can(var.settings.shutdown_recovery_group.pre_action) ? [var.settings.shutdown_recovery_group.pre_action] : []
+        for_each = can(try(var.settings.shutdown_recovery_group, {}).pre_action)
+          ? [try(var.settings.shutdown_recovery_group, {}).pre_action]
+          : []
         content {
           name                      = pre_action.value.name
-          type                      = pre_action.value.type                       # (Required) Type of the action detail. Possible values are AutomationRunbookActionDetails, ManualActionDetails and ScriptActionDetails.
-          fail_over_directions      = pre_action.value.fail_over_directions       # (Required) Directions of fail over. Possible values are PrimaryToRecovery and RecoveryToPrimary
-          fail_over_types           = pre_action.value.fail_over_types            # (Required) Types of fail over. Possible values are TestFailover, PlannedFailover and UnplannedFailover
-          fabric_location           = try(pre_action.value.fabric_location, null) # (Optional) The fabric location of runbook or script. Possible values are Primary and Recovery. It must not be specified when type is ManualActionDetails.
+          type                      = pre_action.value.type
+          fail_over_directions      = pre_action.value.fail_over_directions
+          fail_over_types           = pre_action.value.fail_over_types
+          fabric_location           = try(pre_action.value.fabric_location, null)
           runbook_id                = try(pre_action.value.runbook_id, null)
           manual_action_instruction = try(pre_action.value.manual_action_instruction, null)
           script_path               = try(pre_action.value.script_path, null)
@@ -24,13 +26,15 @@ resource "azurerm_site_recovery_replication_recovery_plan" "replication_plan" {
       }
 
       dynamic "post_action" {
-        for_each = can(var.settings.shutdown_recovery_group.post_action) ? [var.settings.shutdown_recovery_group.post_action] : []
+        for_each = can(try(var.settings.shutdown_recovery_group, {}).post_action)
+          ? [try(var.settings.shutdown_recovery_group, {}).post_action]
+          : []
         content {
           name                      = post_action.value.name
-          type                      = post_action.value.type                       # (Required) Type of the action detail. Possible values are AutomationRunbookActionDetails, ManualActionDetails and ScriptActionDetails.
-          fail_over_directions      = post_action.value.fail_over_directions       # (Required) Directions of fail over. Possible values are PrimaryToRecovery and RecoveryToPrimary
-          fail_over_types           = post_action.value.fail_over_types            # (Required) Types of fail over. Possible values are TestFailover, PlannedFailover and UnplannedFailover
-          fabric_location           = try(post_action.value.fabric_location, null) # (Optional) The fabric location of runbook or script. Possible values are Primary and Recovery. It must not be specified when type is ManualActionDetails.
+          type                      = post_action.value.type
+          fail_over_directions      = post_action.value.fail_over_directions
+          fail_over_types           = post_action.value.fail_over_types
+          fabric_location           = try(post_action.value.fabric_location, null)
           runbook_id                = try(post_action.value.runbook_id, null)
           manual_action_instruction = try(post_action.value.manual_action_instruction, null)
           script_path               = try(post_action.value.script_path, null)
@@ -39,17 +43,20 @@ resource "azurerm_site_recovery_replication_recovery_plan" "replication_plan" {
     }
   }
 
+
   dynamic "failover_recovery_group" {
-    for_each = try(var.settings.failover_recovery_group, null) != null ? [var.settings.failover_recovery_group] : []
+    for_each = [1]
     content {
       dynamic "pre_action" {
-        for_each = can(var.settings.failover_recovery_group.pre_action) ? [var.settings.failover_recovery_group.pre_action] : []
+        for_each = can(try(var.settings.failover_recovery_group, {}).pre_action)
+          ? [try(var.settings.failover_recovery_group, {}).pre_action]
+          : []
         content {
           name                      = pre_action.value.name
-          type                      = pre_action.value.type                       # (Required) Type of the action detail. Possible values are AutomationRunbookActionDetails, ManualActionDetails and ScriptActionDetails.
-          fail_over_directions      = pre_action.value.fail_over_directions       # (Required) Directions of fail over. Possible values are PrimaryToRecovery and RecoveryToPrimary
-          fail_over_types           = pre_action.value.fail_over_types            # (Required) Types of fail over. Possible values are TestFailover, PlannedFailover and UnplannedFailover
-          fabric_location           = try(pre_action.value.fabric_location, null) # (Optional) The fabric location of runbook or script. Possible values are Primary and Recovery. It must not be specified when type is ManualActionDetails.
+          type                      = pre_action.value.type
+          fail_over_directions      = pre_action.value.fail_over_directions
+          fail_over_types           = pre_action.value.fail_over_types
+          fabric_location           = try(pre_action.value.fabric_location, null)
           runbook_id                = try(pre_action.value.runbook_id, null)
           manual_action_instruction = try(pre_action.value.manual_action_instruction, null)
           script_path               = try(pre_action.value.script_path, null)
@@ -57,13 +64,15 @@ resource "azurerm_site_recovery_replication_recovery_plan" "replication_plan" {
       }
 
       dynamic "post_action" {
-        for_each = can(var.settings.failover_recovery_group.post_action) ? [var.settings.failover_recovery_group.post_action] : []
+        for_each = can(try(var.settings.failover_recovery_group, {}).post_action)
+          ? [try(var.settings.failover_recovery_group, {}).post_action]
+          : []
         content {
           name                      = post_action.value.name
-          type                      = post_action.value.type                       # (Required) Type of the action detail. Possible values are AutomationRunbookActionDetails, ManualActionDetails and ScriptActionDetails.
-          fail_over_directions      = post_action.value.fail_over_directions       # (Required) Directions of fail over. Possible values are PrimaryToRecovery and RecoveryToPrimary
-          fail_over_types           = post_action.value.fail_over_types            # (Required) Types of fail over. Possible values are TestFailover, PlannedFailover and UnplannedFailover
-          fabric_location           = try(post_action.value.fabric_location, null) # (Optional) The fabric location of runbook or script. Possible values are Primary and Recovery. It must not be specified when type is ManualActionDetails.
+          type                      = post_action.value.type
+          fail_over_directions      = post_action.value.fail_over_directions
+          fail_over_types           = post_action.value.fail_over_types
+          fabric_location           = try(post_action.value.fabric_location, null)
           runbook_id                = try(post_action.value.runbook_id, null)
           manual_action_instruction = try(post_action.value.manual_action_instruction, null)
           script_path               = try(post_action.value.script_path, null)
