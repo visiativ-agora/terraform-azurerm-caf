b8950869844449e54a5efd884e8f7bfc0d1ec130
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-03-03 17:04:43 +0100

    Refactor application_insight references in locals: update variable names for consistency and clarity

--- modules/webapps/linux_web_app/locals_linux_web_app.tf
+++ modules/webapps/linux_web_app/locals_linux_web_app.tf
@@ -4,14 +4,14 @@ locals {
   app_settings = merge(
     try(var.settings.application_insight, null) == null ? {} : {
       "APPINSIGHTS_INSTRUMENTATIONKEY" = try(
-        var.remote_objects.application_insights[var.settings.application_insights.lz_key][var.settings.application_insights.key].instrumentation_key,
-        var.remote_objects.application_insights[var.client_config.landingzone_key][var.settings.application_insights.key].instrumentation_key,
-        var.settings.application_insights.instrumentation_key,
+        var.remote_objects.application_insights[var.settings.application_insight.lz_key][var.settings.application_insight.key].instrumentation_key,
+        var.remote_objects.application_insights[var.client_config.landingzone_key][var.settings.application_insight.key].instrumentation_key,
+        var.settings.application_insight.instrumentation_key,
       null),
       "APPLICATIONINSIGHTS_CONNECTION_STRING" = try(
-        var.remote_objects.application_insights[var.settings.application_insights.lz_key][var.settings.application_insights.key].connection_string,
-        var.remote_objects.application_insights[var.client_config.landingzone_key][var.settings.application_insights.key].connection_string,
-        var.settings.application_insights.connection_string,
+        var.remote_objects.application_insights[var.settings.application_insight.lz_key][var.settings.application_insight.key].connection_string,
+        var.remote_objects.application_insights[var.client_config.landingzone_key][var.settings.application_insight.key].connection_string,
+        var.settings.application_insight.connection_string,
       null),
       "ApplicationInsightsAgent_EXTENSION_VERSION" = "~3"
     },
