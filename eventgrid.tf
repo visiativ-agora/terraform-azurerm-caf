1c3547091d3f0f88c07725c61aaefe5fc6266881
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-03-05 10:39:06 +0100

    test

--- eventgrid.tf
+++ eventgrid.tf
@@ -48,7 +48,7 @@ module "eventgrid_event_subscription" {
 
   remote_objects = {
     all                    = local.remote_objects,
-    functions              = local.combined_objects_function_apps,
+    functions              = local.combined_objects_linux_function_apps,
     eventhubs              = local.combined_objects_event_hubs,
     servicebus_topic       = local.combined_objects_servicebus_topics,
     servicebus_queues      = local.combined_objects_servicebus_queues,
@@ -82,16 +82,14 @@ module "eventgrid_system_topic" {
   source   = "./modules/messaging/eventgrid/eventgrid_system_topic"
   for_each = local.messaging.eventgrid_system_topic
 
-  global_settings = local.global_settings
-  client_config   = local.client_config
-  settings        = each.value
-  base_tags       = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
-
-  # location = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
-  location = lookup(each.value, "region", null) != null ? each.value.region : can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
+  global_settings  = local.global_settings
+  client_config    = local.client_config
+  settings         = each.value
+  base_tags        = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
+  location         = lookup(each.value, "region", null) != null ? each.value.region : can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
   combined_objects = local.dynamic_app_settings_combined_objects
   identity         = try(each.value.identity, null)
-  remote_objects = local.remote_objects
+  remote_objects   = local.remote_objects
 }
 output "eventgrid_system_topic" {
   value = module.eventgrid_system_topic
@@ -100,15 +98,15 @@ module "eventgrid_system_event_subscription" {
   source   = "./modules/messaging/eventgrid/eventgrid_system_event_subscription"
   for_each = local.messaging.eventgrid_system_event_subscription
 
-  global_settings = local.global_settings
-  client_config   = local.client_config
-  settings        = each.value
+  global_settings    = local.global_settings
+  client_config      = local.client_config
+  settings           = each.value
   managed_identities = local.combined_objects_managed_identities
 
   remote_objects = merge(
     local.remote_objects,
     {
-      functions               = local.combined_objects_function_apps,
+      functions               = local.combined_objects_linux_function_apps,
       eventhubs               = local.combined_objects_event_hubs,
       eventgrid_system_topics = local.combined_objects_eventgrid_system_topics,
       hybrid_connections      = local.combined_objects_relay_hybrid_connection,
