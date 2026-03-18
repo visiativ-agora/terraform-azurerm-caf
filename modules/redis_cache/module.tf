151e361b81973cd724a610ac442f7070593fa88e
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-03-18 14:27:21 +0100

    Update redis_configuration in azurerm_redis_cache to include data_persistence_authentication_method and adjust existing attributes

--- modules/redis_cache/module.tf
+++ modules/redis_cache/module.tf
@@ -35,20 +35,21 @@ resource "azurerm_redis_cache" "redis" {
     for_each = lookup(var.redis, "redis_configuration", {}) != {} ? [var.redis.redis_configuration] : []
 
     content {
-      aof_backup_enabled              = lookup(redis_configuration.value, "aof_backup_enabled", null)
-      aof_storage_connection_string_0 = lookup(redis_configuration.value, "aof_storage_connection_string_0", null)
-      aof_storage_connection_string_1 = lookup(redis_configuration.value, "aof_storage_connection_string_1", null)
-      authentication_enabled          = lookup(redis_configuration.value, "authentication_enabled", null)
+      aof_backup_enabled                      = lookup(redis_configuration.value, "aof_backup_enabled", null)
+      aof_storage_connection_string_0         = lookup(redis_configuration.value, "aof_storage_connection_string_0", null)
+      aof_storage_connection_string_1         = lookup(redis_configuration.value, "aof_storage_connection_string_1", null)
+      authentication_enabled                  = lookup(redis_configuration.value, "authentication_enabled", null)
       active_directory_authentication_enabled = lookup(redis_configuration.value, "active_directory_authentication_enabled", null)
-      maxfragmentationmemory_reserved = lookup(redis_configuration.value, "maxfragmentationmemory_reserved", null)
-      maxmemory_delta                 = lookup(redis_configuration.value, "maxmemory_delta", null)
-      maxmemory_policy                = lookup(redis_configuration.value, "maxmemory_policy", null)
-      maxmemory_reserved              = lookup(redis_configuration.value, "maxmemory_reserved", null)
-      notify_keyspace_events          = lookup(redis_configuration.value, "notify_keyspace_events", null)
-      rdb_backup_enabled              = lookup(redis_configuration.value, "rdb_backup_enabled", null)
-      rdb_backup_frequency            = lookup(redis_configuration.value, "rdb_backup_frequency", null)
-      rdb_backup_max_snapshot_count   = lookup(redis_configuration.value, "rdb_backup_max_snapshot_count", null)
-      rdb_storage_connection_string   = lookup(redis_configuration.value, "rdb_storage_connection_string", null)
+      data_persistence_authentication_method  = lookup(redis_configuration.value, "data_persistence_authentication_method", null)
+      maxfragmentationmemory_reserved         = lookup(redis_configuration.value, "maxfragmentationmemory_reserved", null)
+      maxmemory_delta                         = lookup(redis_configuration.value, "maxmemory_delta", null)
+      maxmemory_policy                        = lookup(redis_configuration.value, "maxmemory_policy", null)
+      maxmemory_reserved                      = lookup(redis_configuration.value, "maxmemory_reserved", null)
+      notify_keyspace_events                  = lookup(redis_configuration.value, "notify_keyspace_events", null)
+      rdb_backup_enabled                      = lookup(redis_configuration.value, "rdb_backup_enabled", null)
+      rdb_backup_frequency                    = lookup(redis_configuration.value, "rdb_backup_frequency", null)
+      rdb_backup_max_snapshot_count           = lookup(redis_configuration.value, "rdb_backup_max_snapshot_count", null)
+      rdb_storage_connection_string           = lookup(redis_configuration.value, "rdb_storage_connection_string", null)
     }
   }
 
