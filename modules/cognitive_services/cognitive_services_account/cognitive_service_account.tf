94c8f671b502f20b4f3430d4ccfc7aa732fabd89
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-03-18 10:42:14 +0100

    Update bypass handling in network ACLs to default to null

--- modules/cognitive_services/cognitive_services_account/cognitive_service_account.tf
+++ modules/cognitive_services/cognitive_services_account/cognitive_service_account.tf
@@ -51,7 +51,7 @@ resource "azurerm_cognitive_account" "service" {
     content {
       default_action = network_acls.value.default_action
       ip_rules       = try(network_acls.value.ip_rules, [])
-      bypass         = try(network_acls.value.bypass, "None") 
+      bypass         = try(network_acls.value.bypass, null) 
 
       dynamic "virtual_network_rules" {
         for_each = try(network_acls.value.subnets, try(network_acls.value.virtual_network_rules, {}))
